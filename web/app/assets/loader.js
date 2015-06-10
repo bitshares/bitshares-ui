96812a9b060eb711495ec3c90b4b23254c4c8f2c
valzav <i@valzav.com>
2015-06-10 17:04:39 -0400

    added favicon

--- web/app/assets/loader.js
+++ web/app/assets/loader.js
@@ -1,5 +1,6 @@
 require("./stylesheets/app.scss");
 require("file?name=index.html!./index.html");
+require("file?name=favicon.ico!./favicon.ico");
 
 var fonts = ["Roboto-Bold", "Roboto-Light", "Roboto-Regular", "RobotoCondensed-Regular"];
 var fonts_ext = ["eot", "svg", "ttf", "woff"];
