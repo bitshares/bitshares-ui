9ce799b533b16043f3745347c4d532040e100ba9
svk31 <sigvekvalsvik@gmail.com>
2015-06-11 10:50:27 +0200

    Prevent multiple requests for same assets in AssetActions

--- dl/src/actions/AssetActions.js
+++ dl/src/actions/AssetActions.js
@@ -2,6 +2,8 @@ var alt = require("../alt-instance");
 import Apis from "rpc_api/ApiInstances";
 import utils from "common/utils";
 
+let inProgress = {};
+
 class AssetActions {
 
     createAsset(assetObject) {
@@ -9,30 +11,44 @@ class AssetActions {
     }
 
     getAssetList(start, count) {
-        Apis.instance().db_api().exec("list_assets", [
-            start, count
-        ]).then(result => {
-            this.dispatch(result);
-        })
+        console.log("get assetlist:", start, count);
+        let id = start + "_" + count;
+        if (!inProgress[id]) {
+            Apis.instance().db_api().exec("list_assets", [
+                start, count
+            ]).then(result => {
+                this.dispatch(result);
+                delete inProgress[id];
+            })
+            .catch(error => {
+                console.log("Error in AssetStore.getAssetList: ", error);
+                delete inProgress[id];
+            });
+        }
     }
 
     getAsset(id) {
         let assetPromise;
-        if (utils.is_object_id(id)) {
-            assetPromise = Apis.instance().db_api().exec("get_objects", [
-                [id]
-            ]);
-        } else {
-            assetPromise = Apis.instance().db_api().exec("list_assets", [
-                id, 1
-            ]);
-        }
+        if (!inProgress[id]) {
+            inProgress[id] = true;
+            if (utils.is_object_id(id)) {
+                assetPromise = Apis.instance().db_api().exec("get_objects", [
+                    [id]
+                ]);
+            } else {
+                assetPromise = Apis.instance().db_api().exec("list_assets", [
+                    id, 1
+                ]);
+            }
 
-        return assetPromise.then((result) => {
-            this.dispatch(result[0]);
-        }).catch((error) => {
-            console.log("Error in AssetStore.updateAsset: ", error);
-        });
+            return assetPromise.then((result) => {
+                this.dispatch(result[0]);
+                delete inProgress[id];
+            }).catch((error) => {
+                console.log("Error in AssetStore.updateAsset: ", error);
+                delete inProgress[id];
+            });
+        }
     }
 }
 
