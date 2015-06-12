751691b6472461dac9c01fa2a75ff65530418207
svk31 <sigvekvalsvik@gmail.com>
2015-06-12 16:39:31 +0200

    Refactor Block.jsx

--- web/app/components/Blockchain/Block.jsx
+++ web/app/components/Blockchain/Block.jsx
@@ -11,6 +11,71 @@ import BlockchainActions from "actions/BlockchainActions";
 import Transaction from "./Transaction";
 import Translate from "react-translate-component";
 
+class TransactionList extends React.Component {
+
+    shouldComponentUpdate(nextProps) {
+        // console.log("witnesses:", !Immutable.is(nextProps.witnesses, this.props.witnesses))
+        // console.log("witness_id_to_name:", !Immutable.is(nextProps.witness_id_to_name, this.props.witness_id_to_name))
+        // console.log("assets:", !Immutable.is(nextProps.assets, this.props.assets))
+        return (
+                nextProps.block.id !== this.props.block.id ||
+                !Immutable.is(nextProps.witnesses, this.props.witnesses) ||
+                !Immutable.is(nextProps.witness_id_to_name, this.props.witness_id_to_name) ||
+                !Immutable.is(nextProps.assets, this.props.assets)
+            );
+    }
+
+    _fetchWitnesses(witnessIds, witnesses, witness_id_to_name) {
+        if (!Array.isArray(witnessIds)) {
+            witnessIds = [witnessIds];
+        }
+        let missing = [];
+        let missingAccounts = [];
+        witnessIds.forEach(id => {
+            // Check for missing witness data
+            if (!witnesses.get(id)) {
+                missing.push(id);
+            // Check for missing witness account data
+            } else if (!witness_id_to_name.get(id)) {
+                missingAccounts.push(witnesses.get(id).witness_account);
+            }
+        });
+
+        if (missing.length > 0) {
+            WitnessActions.getWitnesses(missing);
+        } 
+
+        if (missingAccounts.length > 0) {
+            WitnessActions.getWitnessAccounts(missingAccounts);
+        }
+    }
+
+    render() {
+        let {block, assets, accounts, witnesses, witness_id_to_name} = this.props;
+        let transactions = null;
+        
+        transactions = [];
+
+        if (!witness_id_to_name.get(block.witness)) {
+            this._fetchWitnesses(block.witness, witnesses, witness_id_to_name);
+        }
+        
+        if (block.transactions.length > 0) {
+            transactions = [];
+
+            block.transactions.forEach((trx, index) => {
+                transactions.push(<Transaction key={index} trx={trx} assets={assets} accounts={accounts} index={index}/>);
+            });
+        }
+
+        return (
+                <div>
+                    {transactions}
+                </div>
+            );
+    }
+}
+
 class Block extends BaseComponent {
     constructor(props) {
         super(props);
@@ -29,7 +94,6 @@ class Block extends BaseComponent {
             !Immutable.is(nextProps.witnesses, this.props.witnesses) ||
             !Immutable.is(nextProps.witness_id_to_name, this.props.witness_id_to_name) ||
             !Immutable.is(nextProps.assets, this.props.assets) ||
-            nextProps.accounts !== this.props.accounts ||
             nextProps.dynGlobalObject !== this.props.dynGlobalObject
             );
     }
@@ -62,38 +126,9 @@ class Block extends BaseComponent {
     }
 
     componentDidMount() {
-        if (!this.props.assets.get("1.4.0")) {
-            AssetActions.getAsset("1.4.0");
-        }
         this._getBlock(this.props.height);
         this._bindKey("right", this._nextBlock);
         this._bindKey("left", this._previousBlock);
-    }
-
-    _fetchWitnesses(witnessIds, witnesses, witness_id_to_name) {
-        if (!Array.isArray(witnessIds)) {
-            witnessIds = [witnessIds];
-        }
-        let missing = [];
-        let missingAccounts = [];
-        witnessIds.forEach(id => {
-            // Check for missing witness data
-            if (!witnesses.get(id)) {
-                missing.push(id);
-            // Check for missing witness account data
-            } else if (!witness_id_to_name.get(id)) {
-                missingAccounts.push(witnesses.get(id).witness_account);
-            }
-        });
-
-        if (missing.length > 0) {
-            WitnessActions.getWitnesses(missing);
-        } 
-
-        if (missingAccounts.length > 0) {
-            WitnessActions.getWitnessAccounts(missingAccounts);
-        }
-        // }
     }
 
     render() {
@@ -101,23 +136,6 @@ class Block extends BaseComponent {
         let {blocks, assets, accounts, witnesses, witness_id_to_name} = this.props;
         let height = parseInt(this.props.height, 10);
         let block = blocks.get(height);
-        let transactions = null;
-
-        if (block) {
-            // console.log("block:", block);
-            if (!witness_id_to_name.get(block.witness)) {
-                this._fetchWitnesses(block.witness, witnesses, witness_id_to_name);
-            }
-            
-            if (block.transactions.length > 0) {
-                transactions = [];
-
-                block.transactions.forEach((trx, index) => {
-                    transactions.push(<Transaction key={index} trx={trx} assets={assets} accounts={accounts} index={index}/>);
-                });
-
-            }
-        }
 
         return (
             <div className="grid-block small-offset-2">
@@ -137,10 +155,10 @@ class Block extends BaseComponent {
                         <li><Translate component="span" content="explorer.block.previous" />: {block.previous}</li>
                         <li><Translate component="span" content="explorer.block.previous_secret" />: {block.previous_secret}</li>
                         <li><Translate component="span" content="explorer.block.next_secret" />: {block.next_secret_hash}</li>
+                        <li><Translate component="span" content="explorer.block.transactions" />: {block.transactions.length}</li>
                     </ul>
                     ) : null}
-
-                    {transactions ? transactions : null}
+                    {block ? <TransactionList assets={assets} accounts={accounts} block={block} witnesses={witnesses} witness_id_to_name={witness_id_to_name}/> : null}
                 </div>
             </div>
         );
