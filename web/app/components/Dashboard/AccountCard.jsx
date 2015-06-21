1ccdd47f20d888c647953c7fa4b3350b5f023b93
svk31 <sigvekvalsvik@gmail.com>
2015-06-21 10:49:19 +0200

    Refactor balance treemap charts into separate component

--- web/app/components/Dashboard/AccountCard.jsx
+++ web/app/components/Dashboard/AccountCard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import AccountImage from "../Account/AccountImage";
 import AccountActions from "actions/AccountActions";
-import FormattedAsset from "../Utility/FormattedAsset";
 import validation from "common/validation.coffee";
 import {Link} from "react-router";
-import Highcharts from "react-highcharts";
-import utils from "common/utils";
+import BalanceTreemap from "./BalanceTreemap";
 
 require("../../assets/heatmap");
 require("../../assets/treemap");
@@ -72,80 +70,6 @@ class AccountCard extends React.Component {
         }
 
         let {account, assets, balances} = this.props;
-        let accountBalances = null;
-        // let accountBalances = balances.map((balance) => {
-        //     return (
-        //         <div key={balance.asset_id} className="text-group">
-        //             <span>
-        //                 <FormattedAsset amount={parseFloat(balance.amount)} asset={assets.get(balance.asset_id)}/>
-        //             </span>
-        //         </div>);
-        // });
-
-        if (balances && balances.length > 0 && assets.size > 0) {
-            accountBalances = balances.map((balance, index) => {
-                let asset = assets.get(balance.asset_id);
-                return {
-                        name: asset.symbol,
-                        value: utils.get_asset_amount(balance.amount, asset),
-                        colorValue: index
-                    };
-
-            });
-        }
-
-        if (accountBalances && accountBalances.length === 1 && accountBalances[0].value === 0) {
-            accountBalances = null;
-        }
-
-        let config = {
-            chart: {
-                backgroundColor: "rgba(255, 0, 0, 0)",
-                height: 125,
-                spacingLeft: 0,
-                spacingRight: 0,
-                spacingBottom: 0
-            },
-            colorAxis: {
-                minColor: "#FCAB53",
-                maxColor: "#50D2C2"
-            },
-            credits: {
-                enabled: false
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                treemap: {
-                    animation: false,
-                    tooltip: {
-                        pointFormat: "<b>{point.name}</b>: {point.value:,.0f}"
-                    }
-                }
-            },
-            series: [{
-                type: "treemap",
-                layoutAlgorithm: "sliceAndDice",
-                levels: [{
-                    level: 1,
-                    layoutAlgorithm: "sliceAndDice",
-                    dataLabels: {
-                        enabled: true,
-                        align: "left",
-                        verticalAlign: "top",
-                        style: {
-                            fontSize: "13px",
-                            fontWeight: "bold"
-                        }
-                    }
-                }],
-                data: accountBalances
-        }],
-            title: {
-                text: null
-            }
-        };
 
         return (
             <div style={{padding: "0.5em 0.5em"}} className="grid-content account-card">
@@ -158,7 +82,7 @@ class AccountCard extends React.Component {
                             {account.name}
                         </div>
                         <div className="card-section" style={{padding: 0}}>
-                            {accountBalances ? <Highcharts config={config}/> : null}
+                            <BalanceTreemap assets={assets} balances={balances}/>
                         </div>
                     </Link>
                 </div>
---
+++ web/app/components/Dashboard/BalanceTreemap.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import Immutable from "immutable";
+import Highcharts from "react-highcharts";
+import {PropTypes} from "react";
+import utils from "common/utils";
+
+class BalanceTreemap extends React.Component {
+
+    shouldComponentUpdate(nextProps) {
+
+        return (
+            !Immutable.is(nextProps.assets, this.props.assets) ||
+            !nextProps.balances.equals(this.props.balances)
+        );
+    }
+
+    render() {
+        let {assets, balances} = this.props;
+        let accountBalances = null;
+
+        if (balances && balances.length > 0 && assets.size > 0) {
+            accountBalances = balances.map((balance, index) => {
+                let asset = assets.get(balance.asset_id);
+                return {
+                        name: asset.symbol,
+                        value: utils.get_asset_amount(balance.amount, asset),
+                        colorValue: index
+                    };
+
+            });
+        }
+
+        if (accountBalances && accountBalances.length === 1 && accountBalances[0].value === 0) {
+            accountBalances = null;
+        }
+
+        let config = {
+            chart: {
+                backgroundColor: "rgba(255, 0, 0, 0)",
+                height: 125,
+                spacingLeft: 0,
+                spacingRight: 0,
+                spacingBottom: 0
+            },
+            colorAxis: {
+                minColor: "#FCAB53",
+                maxColor: "#50D2C2"
+            },
+            credits: {
+                enabled: false
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                treemap: {
+                    animation: false,
+                    tooltip: {
+                        pointFormat: "<b>{point.name}</b>: {point.value:,.0f}"
+                    }
+                }
+            },
+            series: [{
+                type: "treemap",
+                levels: [{
+                    level: 1,
+                    layoutAlgorithm: "sliceAndDice",
+                    dataLabels: {
+                        enabled: true,
+                        align: "center",
+                        verticalAlign: "middle",
+                        style: {
+                            fontSize: "13px",
+                            fontWeight: "bold"
+                        }
+                    }
+                }],
+                data: accountBalances
+        }],
+            title: {
+                text: null
+            }
+        };
+
+        return (
+            accountBalances ? <Highcharts config={config}/> : null
+        );
+    }
+}
+
+BalanceTreemap.defaultProps = {
+    assets: {},
+    balances: {}
+};
+
+BalanceTreemap.propTypes = {
+    assets: PropTypes.object.isRequired,
+    balances: PropTypes.object.isRequired
+};
+
+export default BalanceTreemap;
