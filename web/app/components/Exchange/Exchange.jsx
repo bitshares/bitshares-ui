f75b0dcde680029c59b42f4f79cb60816dfdef60
valzav <i@valzav.com>
2015-06-23 18:01:25 -0400

    fixing exchange page layout

--- web/app/components/Exchange/Exchange.jsx
+++ web/app/components/Exchange/Exchange.jsx
@@ -11,12 +11,12 @@ require("./exchange.scss");
 
 let history = {
     orders: [
-        {timestamp: new Date(15,6,1,11,38,0,0), type: 0, amount: 5, price: 150},
-        {timestamp: new Date(15,6,1,11,37,0,0), type: 0, amount: 10, price: 152},
-        {timestamp: new Date(15,6,1,11,36,0,0), type: 1, amount: 1, price: 155},
-        {timestamp: new Date(15,6,1,11,35,0,0), type: 1, amount: 80, price: 154},
-        {timestamp: new Date(15,6,1,11,34,0,0), type: 1, amount: 1, price: 148},
-        {timestamp: new Date(15,6,1,11,33,0,0), type: 0, amount: 1, price: 145}
+        {timestamp: new Date(15, 6, 1, 11, 38, 0, 0), type: 0, amount: 5, price: 150},
+        {timestamp: new Date(15, 6, 1, 11, 37, 0, 0), type: 0, amount: 10, price: 152},
+        {timestamp: new Date(15, 6, 1, 11, 36, 0, 0), type: 1, amount: 1, price: 155},
+        {timestamp: new Date(15, 6, 1, 11, 35, 0, 0), type: 1, amount: 80, price: 154},
+        {timestamp: new Date(15, 6, 1, 11, 34, 0, 0), type: 1, amount: 1, price: 148},
+        {timestamp: new Date(15, 6, 1, 11, 33, 0, 0), type: 0, amount: 1, price: 145}
     ]
 };
 
@@ -94,10 +94,13 @@ class Exchange extends React.Component {
         MarketsActions.unSubscribeMarket(quote_id, base_id);
     }
 
-    _buyAmountChanged(e) { this.setState({buyAmount: e.target.value }); }
-    _buyPriceChanged(e) { this.setState({buyPrice: e.target.value }); }
-    _sellAmountChanged(e) { this.setState({sellAmount: e.target.value }); }
-    _sellPriceChanged(e) { this.setState({sellPrice: e.target.value }); }
+    _buyAmountChanged(e) { this.setState({buyAmount: e.target.value}); }
+
+    _buyPriceChanged(e) { this.setState({buyPrice: e.target.value}); }
+
+    _sellAmountChanged(e) { this.setState({sellAmount: e.target.value}); }
+
+    _sellPriceChanged(e) { this.setState({sellPrice: e.target.value}); }
 
     _changeTab(value) {
         this.setState({activeTab: value});
@@ -134,147 +137,139 @@ class Exchange extends React.Component {
 
         let buyForm = (
             <form className="order-form"
-                onSubmit={this._createLimitOrder.bind(this, quote, base, this.state.buyAmount, this.state.buyAmount * this.state.buyPrice)}>
+                  onSubmit={this._createLimitOrder.bind(this, quote, base, this.state.buyAmount, this.state.buyAmount * this.state.buyPrice)}>
                 <label>
                     Quantity ({quoteSymbol}):
-                    <input type="text" id="buyAmount" value={this.state.buyAmount} onChange={this._buyAmountChanged.bind(this)} />
+                    <input type="text" id="buyAmount" value={this.state.buyAmount} onChange={this._buyAmountChanged.bind(this)}/>
                 </label>
                 <label>
                     Price: ({baseSymbol} per {quoteSymbol}):
-                    <input type="text" id="buyPrice" value={this.state.buyPrice} onChange={this._buyPriceChanged.bind(this)} />
+                    <input type="text" id="buyPrice" value={this.state.buyPrice} onChange={this._buyPriceChanged.bind(this)}/>
                 </label>
+
                 <p>Total ({baseSymbol}): { buyTotal }</p>
-                <input type="submit" className="button" value={"Buy " + quoteSymbol} />
+                <input type="submit" className="button" value={"Buy " + quoteSymbol}/>
             </form>
         );
 
         let sellForm = (
             <form className="order-form"
-                onSubmit={this._createLimitOrder.bind(this, base, quote, this.state.sellAmount * this.state.sellPrice, this.state.sellAmount)}>
+                  onSubmit={this._createLimitOrder.bind(this, base, quote, this.state.sellAmount * this.state.sellPrice, this.state.sellAmount)}>
                 <label>
                     Quantity ({quoteSymbol}):
-                    <input type="text" id="sellAmount" value={this.state.sellAmount} onChange={this._sellAmountChanged.bind(this)} />
+                    <input type="text" id="sellAmount" value={this.state.sellAmount} onChange={this._sellAmountChanged.bind(this)}/>
                 </label>
                 <label>
                     Price: ({baseSymbol} per {quoteSymbol}):
-                    <input type="text" id="sellPrice" value={this.state.sellPrice} onChange={this._sellPriceChanged.bind(this)} />
+                    <input type="text" id="sellPrice" value={this.state.sellPrice} onChange={this._sellPriceChanged.bind(this)}/>
                 </label>
+
                 <p>Total ({baseSymbol}): { sellTotal }</p>
-                <input type="submit" className="button" value={"Sell " + quoteSymbol} />
+                <input type="submit" className="button" value={"Sell " + quoteSymbol}/>
             </form>
         );
 
         return (
-            <div className="grid-block">
+            <div className="grid-block page-layout small-vertical medium-horizontal" style={{flexWrap: "nowrap"}}>
 
-                <div className="grid-block page-layout small-vertical medium-horizontal" style={{flexWrap: "nowrap"}}>
-
-                    {/* Left Column */}
-                    <div className="grid-block shrink left-column">
-                        <div className="grid-content">
-
+                {/* Left Column */}
+                <div className="grid-block left-column-2">
+                    <div className="grid-content">
                         {/* Order Book */}
-                        <div className="grid-block shrink left-column-2">
-                            <div className="grid-content">
-                                <OrderBook
-                                    orders={limit_orders}
-                                    base={base}
-                                    quote={quote}
-                                    baseSymbol={baseSymbol}
-                                    quoteSymbol={quoteSymbol}
+                        <OrderBook
+                            orders={limit_orders}
+                            base={base}
+                            quote={quote}
+                            baseSymbol={baseSymbol}
+                            quoteSymbol={quoteSymbol}
+                            />
+                    </div>
+                </div>
+
+                {/* Center Column */}
+                <div className="grid-block main-content vertical">
+                    {/* Header */}
+                    <div className="grid-block">
+                        <h2>{baseSymbol} / {quoteSymbol}</h2>
+                    </div>
+
+                    {/* Depth Chart */}
+                    <div className="grid-block">
+                        <div className="show-for-large">
+                            <DepthHighChart
+                                orders={limit_orders}
+                                flat_asks={this.props.flat_asks}
+                                flat_bids={this.props.flat_bids}
+                                base={base}
+                                quote={quote}
+                                baseSymbol={baseSymbol}
+                                quoteSymbol={quoteSymbol}
                                 />
-                                </div>
+                        </div>
+                    </div>
+
+                    <div className="grid-block">
+
+                        <div className="grid-block align-spaced">
+                            <div className="grid-content small-4">
+                                {buyForm}
+                            </div>
+                            <div className="grid-content small-4">
+                                {buyForm}
                             </div>
                         </div>
                     </div>
 
-                    {/* Center Column */}
-                    <div className="grid-block main-content vertical ">
-                        <div className="grid-content">
-                            {/* Header */}
-                            <div className="grid-block">
-                                <div className="grid-content">
-                                    <h2>{baseSymbol} / {quoteSymbol}</h2>
-                                </div>
-                            </div>
-
-                            {/* Depth Chart */}
-                            <div className="grid-block show-for-large shrink">
-                                <DepthHighChart
+                    <div className="grid-block">
+                        {/* My Open Orders */}
+                        <div className="grid-block small-6">
+                            <div className="grid-content order-table">
+                                <p>OPEN ORDERS</p>
+                                <MyOpenOrders
                                     orders={limit_orders}
-                                    flat_asks={this.props.flat_asks}
-                                    flat_bids={this.props.flat_bids}
+                                    account={account.id}
                                     base={base}
                                     quote={quote}
                                     baseSymbol={baseSymbol}
                                     quoteSymbol={quoteSymbol}
-                                />
-                            </div>
-
-                            
-                            <div className="grid-block">
-                                
-                                <div className="grid-block align-spaced">
-                                    <div className="grid-content small-4">
-                                        {buyForm}
-                                    </div>
-                                    <div className="grid-content small-4">
-                                        {buyForm}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="grid-block">
-                                {/* My Open Orders */}
-                                <div className="grid-block small-6">
-                                    <div className="grid-content order-table">
-                                        <p>OPEN ORDERS</p>
-                                        <MyOpenOrders
-                                            orders={limit_orders}
-                                            account={account.id}
-                                            base={base}
-                                            quote={quote}
-                                            baseSymbol={baseSymbol}
-                                            quoteSymbol={quoteSymbol}
-                                            onCancel={this._cancelLimitOrder.bind(this)} />
-                                    </div>
-                                </div>
-
-                                {/* My Recent Trades */}
-                                <div className="grid-block small-6">
-                                    <div className="grid-content order-table">
-                                        <p>RECENT TRADES</p>
-                                        <p>TODO: put the user&apos;s most recently completed trades in this panel.</p>
-                                    </div>
-                                </div>
+                                    onCancel={this._cancelLimitOrder.bind(this)}/>
                             </div>
                         </div>
-                        
-                    </div>
 
+                        {/* My Recent Trades */}
+                        <div className="grid-block small-6">
+                            <div className="grid-content order-table">
+                                <p>RECENT TRADES</p>
 
-                    {/* Market History */}
-                    <div className="grid-block shrink right-column">
-                        <div className="grid-content">
-                            <table style={{width: "100%"}} className="table expand order-table">
-                              <p>MARKET HISTORY</p>
-                                <thead>
-                                <tr>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                    <th>Time</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {
-                                    this.state.history.orders.map(orderHistoryEntry)
-                                }
-                                </tbody>
-                            </table>
+                                <p>TODO: put the user&apos;s most recently completed trades in this panel.</p>
+                            </div>
                         </div>
                     </div>
 
                 </div>
+
+
+                {/* Market History */}
+                <div className="grid-block shrink right-column">
+                    <div className="grid-content">
+                        <table style={{width: "100%"}} className="table expand order-table">
+                            <p>MARKET HISTORY</p>
+                            <thead>
+                            <tr>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                                <th>Time</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                this.state.history.orders.map(orderHistoryEntry)
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
             </div>
         );
     }
