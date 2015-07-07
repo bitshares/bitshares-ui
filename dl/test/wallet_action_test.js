843813d7b6223a2524c91ab21467778ac63fe658
James Calfee <james@jcalfee.info>
2015-07-07 08:01:08 -0500

    First es6 test.  Attempt to import a store class

--- dl/test/wallet_action_test.js
+++ dl/test/wallet_action_test.js
@@ -1,19 +1,23 @@
 
-// ./node_modules/.bin/mocha --compilers js:mocha-babel,js:mocha-traceur,coffee:coffee-script --require coffee-script/register test/wallet_action_test.js -g wallet_action_test --watch
+// npm install mocha-babel --save-dev
+// ./node_modules/.bin/mocha --compilers js:mocha-babel,js:mocha-traceur,coffee:coffee-script --require coffee-script/register test/wallet_action_test.js --watch
+
 try {
-    import WalletActions from "stores/WalletActions"
+    //import WalletActions from "stores/WalletActions"
+    //import WalletActions from "./src/stores/WalletActions"
+    //import WalletActions from "../src/stores/WalletActions"
+    //import WalletActions from "../src/stores/WalletActions.js"
     console.log('... WalletAction',WalletAction)
 }catch(e) {
     console.log('...',e)
 }
+/**/
 
-/*
-describe( "wallet_action_tests", ()=> {
-
-    var broadcast = process.env.GPH_TEST_NO_BROADCAST is undefined
-    //var genesis_private = PrivateKey.fromWif("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3")
-    var api = null
+describe( "wallet_actions", ()=> {
     
+    it( "wallet_create", ()=> {
+    })
+/*
     before( (done) => {
         api = ApiInstances.instance()
         api.init_promise.then ()->
@@ -23,4 +27,5 @@ describe( "wallet_action_tests", ()=> {
     after( ()=> {
         api.close()
     })
-})*/
+*/
+})
