acc10cfe4fd331ff03db3407c37b705a5bdd9da2
cass <cass@bitshares.org>
2015-07-13 11:34:16 +0200

    partial german translation

---
+++ web/app/assets/locales/locale-de.js
@@ -0,0 +1,240 @@
+module.exports = {
+    languages: {
+        en: "English",
+        cn: "简体中文",
+        fr: "French",
+        ko: "한국어",
+        de: "German",
+        switch: "Switch language"
+    },
+    header: {
+        title: "Graphene UI",
+        dashboard: "Dashboard",
+        explorer: "Explorer",
+        exchange: "Börse",
+        payments: "Transaktionen",
+        logout: "Abmeldung",
+        settings: "Einstellungen",
+        current: "Aktives Konto"
+    },
+    account: {
+        asset: "Asset",
+        market_value: "Marktwert",
+        hour_24: "24hr Wechsel",
+        recent: "Letzte Aktivität",
+        name: "Konto Name",
+        member: {
+            stats: "Mitgliederstatistiken",
+            join: "Beitritt am",
+            reg: "Registriert von",
+            ref: "Empfohlen von",
+            referrals: "Empfehlungen",
+            rewards: "Belohnungen",
+            cashback: "Skonto",
+            vested: "Vested"
+        },
+        connections: {
+            known: "Bekannt von",
+            "black": "Schwarzgelistet von"
+        }
+    },
+    transfer: {
+        from: "Von",
+        amount: "Betrag",
+        to: "To",
+        memo: "Notiz",
+        fee: "Gebühr",
+        send: "Senden",
+        final: "Endguthaben",
+        balances: "Guthaben",
+        errors: {
+            req: "Plfichtfeld",
+            pos: "Betrag darf nicht negativ sein",
+            valid: "Bitte geben Sie einen positiven Betrag ein"
+        },
+        back: "ZURÜCK",
+        confirm: "BESTÄTIGEN",
+        broadcast: "Deine Überweisung wurde gesendet",
+        again: "WEITERE ÜBERWEISUNG",
+        see: "ÜBERWEISUNGSÜBERSICHT"
+    },
+    transaction: {
+        sent: "Gesendet",
+        to: "an",
+        received: "Empfangen",
+        from: "von",
+        amount_sell: "Betrag",
+        expiration: "Frist",
+        fill_or: "Fill or kill",
+        min_receive: "Mindestbetrag",
+        seller: "Verkäufer",
+        collateral: "Collateral",
+        coll_ratio: "Initial collateral ratio",
+        coll_maint: "Collateral maintenance ratio",
+        "create_key": "Created a public key",
+        reg_account: "Registered the account",
+        was_reg_account: "registriert von",
+        create_asset: "Neuen Asset erstellen",
+        limit_order: "Placed limit order to sell",
+        limit_order_buy: "Placed limit order to buy",
+        limit_order_cancel: "Cancelled limit order with id",
+        short_order: "Placed short order to sell",
+        short_order_cancel: "Cancelled short with id",
+        at: "at",
+        coll_of: "with collateral of",
+        call_order_update: "Updated call order",
+        upgrade_account: "Upgraded the account to lifetime member",
+        update_account: "Updated account",
+        whitelist_account: "Whitelisted the account",
+        whitelisted_by: "Was whitelisted by the account",
+        transfer_account: "Transferred the account",
+        update_asset: "Updated the asset",
+        update_feed_producers: "Updated the feed producers of asset",
+        feed_producer: "Became a feed producer for the asset",
+        asset_issue: "Issued",
+        was_issued: "Was issued",
+        by: "by",
+        burn_asset: "Burnt",
+        fund_pool: "Funded asset fee pool with",
+        asset_settle: "Requested settlement of",
+        asset_global_settle: "Requested global settlement of",
+        publish_feed: "Published new feed for asset",
+        delegate_create: "Created the delegate",
+        witness_create: "Created the witness",
+        witness_pay: "Withdrew witness pay to account",
+        witness_receive: "Received witness from witness",
+        proposal_create: "Created a proposal",
+        proposal_update: "Updated a proposal",
+        proposal_delete: "Deleted a proposal",
+        withdraw_permission_create: "Gave withdrawal permission for account",
+        withdraw_permission_update: "Updated withdrawal permission for account",
+        withdraw_permission_claim: "Claimed withdrawal permission for account",
+        withdraw_permission_delete: "Deleted withdrawal permissions for account",
+        paid: "Paid",
+        obtain: "to obtain",
+        global_parameters_update: "Updated global parameters",
+        file_write: "Wrote a file",
+        vesting_balance_create: "created vesting balance of",
+        for: "for",
+        vesting_balance_withdraw: "Withdrew vesting balance of",
+        bond_create_offer: "Created bond offer",
+        bond_cancel_offer: "Cancelled bond offer",
+        bond_accept_offer: "Accepted bond offer of",
+        bond_claim_collateral: "Claimed collateral of",
+        bond_pay_collateral: "Paid collateral of",
+        create_worker: "Created a worker with a pay of",
+        custom: "Created a custom operation",
+        order_id: "Order ID",
+        trxTypes: {
+            0: "Überweisung",
+            1: "Limit order",
+            2: "Cancel limit order",
+            3: "Update call order",
+            4: "Create account",
+            5: "Kontoaktualisierung",
+            6: "Account whitelist",
+            7: "Konto Upgrade",
+            8: "Konto Überweisung",
+            9: "Asset erstellen",
+            10: "Asset update",
+            11: "SmartCoin update",
+            12: "Update asset feed producers",
+            13: "Issue asset",
+            14: "Burn asset",
+            15: "Fund asset fee pool",
+            16: "Asset settlement",
+            17: "Global asset settlement",
+            18: "Publish asset feed",
+            19: "Create delegate",
+            20: "Create witness",
+            21: "Witness pay withdrawal",
+            22: "Create proposal",
+            23: "Update proposal",
+            24: "Delete proposal",
+            25: "Create withdrawal permission",
+            26: "Update withdrawal permission",
+            27: "Claim withdrawal permission",
+            28: "Delete withdrawal permission",
+            29: "Fill order",
+            30: "Global parameters update",
+            31: "Create vesting balance",
+            32: "Withdraw vesting balance",
+            33: "Create worker",
+            34: "Custom"
+        }
+    },
+    explorer: {
+        accounts: {
+            title: "Konten"
+        },
+        blocks: {
+            title: "Blockchain",
+            globals: "Globale Einstellungen",
+            recent: "Letzte Blöcke"
+        },
+        block: {
+            title: "Block",
+            id: "Block ID",
+            witness: "Witness",
+            count: "Transaction count",
+            date: "Datum",
+            previous: "Previous",
+            previous_secret: "Previous secret",
+            next_secret: "Next secret hash",
+            op: "Aktion",
+            trx: "Transaktion",
+            op_type: "Aktionstyp",
+            fee_payer: "Fee paying account",
+            key: "Öffentlicher Schlüssel",
+            transactions: "Anzahl der Transaktionen",
+            account_upgrade: "Kontoerweiterung",
+            lifetime: "Lebenslanges Mitglied werden",
+            authorizing_account: "Kontovollmacht",
+            listed_account: "Kontenübersicht",
+            new_listing: "Neuer Eintrag",
+            asset_update: "zu aktualisierender Asset",
+            common_options: "Common Optionen",
+            new_options: "New Option",
+            new_producers: "New feed producers",
+            asset_issue: "Amount to issue",
+            max_margin_period_sec: "Max margin period (s)",
+            call_limit: "Call limit",
+            short_limit: "Short limit",
+            settlement_price: "Settlement price"
+        },
+        assets: {
+            title: "Assets",
+            market: "SmartCoins",
+            user: "User Issued Assets",
+            symbol: "Symbol",
+            id: "ID",
+            issuer: "Issuer",
+            precision: "Genauigkeit"
+        },
+        asset: {
+            title: "Asset"
+        },
+        witnesses: {
+            title: "Witnesses"
+        },
+        delegates: {
+            title: "Delegates"
+        },
+        delegate: {
+            title: "Delegate"
+        },
+        workers: {
+            title: "Workers"
+        },
+        proposals: {
+            title: "Proposals"
+        },
+        account: {
+            title: "Account"
+        }
+    },
+    settings: {
+        inversed: "Market orientation preference",
+        unit: "Preferred unit of account"
+    }
+};
