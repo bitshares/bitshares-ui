892a32e3ec52d24031b495483821bc8cda2a8db6
valzav <i@valzav.com>
2015-07-16 11:54:46 -0700

    unlock wallet modal (related to #60)

--- web/app/components/Wallet/WalletUnlock.jsx
+++ web/app/components/Wallet/WalletUnlock.jsx
@@ -1,42 +1,61 @@
 import React, {Component} from 'react'
-
 import WalletDb from "stores/WalletDb"
 import WalletCreate from "components/Wallet/WalletCreate"
 import NotificationActions from "actions/NotificationActions"
-
 import cname from "classnames"
+import Trigger from "react-foundation-apps/src/trigger";
+import Modal from "react-foundation-apps/src/modal";
+import ZfApi from "react-foundation-apps/src/utils/foundation-api";
 
 export default class WalletUnlock extends Component {
 
     constructor() {
         super()
-        this.state = { 
+        this.state = {
             password_error: null
         }
     }
-    
+
+    componentDidMount() {
+        if (WalletDb.isLocked()) ZfApi.publish("unlock_wallet_modal", "open");
+    }
+
+
     render() {
-        if (WalletDb.isLocked())
-            return <div>
-                <form onSubmit={this._passSubmit.bind(this)}
-                    className={cname(
-                        {"has-error": this.state.password_error}
-                    )}
-                >
-                    <input type="password" placeholder="Wallet Password"
-                        onChange={this._passChange.bind(this)}/>
-                    <div>{this.state.password_error}</div>
-                    <button className="button" onClick={this._passSubmit.bind(this)}>
-                        Unlock Wallet
-                    </button>
-                </form>
-            </div>
-        
+        let modal = null;
+        if (WalletDb.isLocked()) {
+            modal = (
+                <Modal id="unlock_wallet_modal" overlay={true}>
+                    <Trigger close="">
+                        <a href="#" className="close-button">&times;</a>
+                    </Trigger>
+                    <div className="grid-block vertical">
+                        <br/>
+                        <form onSubmit={this._passSubmit.bind(this)}>
+                            <div className="grid-content no-overflow">
+                                <div className={cname("form-group", {"has-error": this.state.password_error})}>
+                                    <label>Wallet Password</label>
+                                    <input type="password" onChange={this._passChange.bind(this)}/>
+                                    <div>{this.state.password_error}</div>
+                                </div>
+                            </div>
+                            <div className="grid-content button-group no-overflow">
+                                <a className="button" href onClick={this._passSubmit.bind(this)}>Unlock Wallet</a>
+                                <Trigger close="unlock_wallet_modal">
+                                    <a href className="secondary button">Cancel</a>
+                                </Trigger>
+                            </div>
+                        </form>
+                    </div>
+                </Modal>
+            );
+        }
         return <div>
+            {modal}
             {this.props.children}
         </div>
     }
-    
+
 //   { ! this.props.relock_button ? "" : <div>
 //               <button className="button" onClick={this._lock.bind(this)}>
 //                   Re-Lock Wallet
@@ -48,22 +67,24 @@ export default class WalletUnlock extends Component {
         this.password_ui = e.target.value
         this.setState({password_error: null})
     }
-    
+
     _passSubmit(e) {
         e.preventDefault()
         WalletDb.validatePassword(
             this.password_ui || "",
             true //unlock
         )
-        if( WalletDb.isLocked())
+        if (WalletDb.isLocked())
             this.setState({password_error: "Wrong password"})
-        else
+        else {
             this.setState({password_error: null})
+            ZfApi.publish("unlock_wallet_modal", "close");
+        }
     }
 
     _lock() {
         WalletDb.onLock()
         this.forceUpdate()
     }
-    
+
 }
