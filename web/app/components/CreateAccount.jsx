622047e569b78f08d4578dc02dd7fc67a9ff7c04
valzav <i@valzav.com>
2015-07-16 17:22:53 -0700

    combining wallet and account creation into the single component

--- web/app/components/CreateAccount.jsx
+++ web/app/components/CreateAccount.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import forms from "newforms";
 import classNames from "classnames";
 import AccountActions from "actions/AccountActions";
-import Wallet from "./Wallet/Wallet";
-import AccountNameInput from "./Forms/AccountNameInput"
-import PasswordInput from "./Forms/PasswordInput"
+import AccountNameInput from "./Forms/AccountNameInput";
+import PasswordInput from "./Forms/PasswordInput";
+import WalletDb from "stores/WalletDb";
+import notify from 'actions/NotificationActions';
 
 class CreateAccount extends React.Component {
     constructor() {
@@ -12,22 +13,12 @@ class CreateAccount extends React.Component {
         this.state = {validAccountName: false};
     }
 
-    onFormChange() {
-        console.log("[CreateAccount.jsx:14] ----- onFormChange ----->");
-        // TODP: validate account name
-        //this.setState({validAccountName: isValid});
-    }
-
     onAccountNameChange(e) {
-        console.log("[CreateAccount.jsx:21] ----- onAccountNameChange ----->", this.refs.account_name.valid());
         this.setState({validAccountName: this.refs.account_name.valid()});
     }
 
-    onSubmit(e) {
-        e.preventDefault();
-        let name = this.refs.account_name.value();
-
-        AccountActions.createAccount(name).then(() => {
+    createAccount(name) {
+        return AccountActions.createAccount(name).then(() => {
             this.props.addNotification({
                 message: `Successfully created account: ${name}`,
                 level: "success",
@@ -45,25 +36,61 @@ class CreateAccount extends React.Component {
         });
     }
 
+    createWallet(password) {
+        return WalletDb.onCreateWallet(
+            this.state.password,
+            null, //this.state.brainkey,
+            true //unlock
+        ).then( ()=> {
+            notify.addNotification({
+                message: `Wallet Created`,
+                level: "success",
+                autoDismiss: 10
+            });
+            this.forceUpdate();
+        }).catch(err => {
+            console.log("CreateWallet failed:", err);
+            notify.addNotification({
+                message: `Failed to create wallet: ${err}`,
+                level: "error",
+                autoDismiss: 10
+            })
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        let name = this.refs.account_name.value();
+
+        if (WalletDb.getWallet()) {
+            this.createAccount(name);
+        } else {
+            let password = this.refs.password.value();
+            this.createWallet(password).then(() => this.createAccount(name));
+        }
+    }
+
     render() {
         let buttonClass = classNames("button", {disabled: !this.state.validAccountName});
 
         return (
-            <div className="grid-block vertical page-layout">
-                <div className="grid-block medium-6">
+            <div className="grid-block vertical">
+                <div className="grid-content">
                     <div className="content-block">
+                        <br/>
+
                         <h3>Create New Account</h3>
                         <br/>
-                        <Wallet>
-                        <form onSubmit={this.onSubmit.bind(this)} onChange={this.onFormChange.bind(this)} noValidate>
+
+                        <form className="medium-3" onSubmit={this.onSubmit.bind(this)} noValidate>
                             <AccountNameInput ref="account_name"
                                               onChange={this.onAccountNameChange.bind(this)}
-                                              accountShouldNotExist={true}
-                                />
-                            {/*<PasswordInput confirmation={true} />*/}
+                                              accountShouldNotExist={true}/>
+                            {WalletDb.getWallet() ? null :
+                                <PasswordInput ref="password" confirmation={true}/>
+                            }
                             <button className={buttonClass}>Create Account</button>
                         </form>
-                        </Wallet>
                     </div>
                 </div>
             </div>
