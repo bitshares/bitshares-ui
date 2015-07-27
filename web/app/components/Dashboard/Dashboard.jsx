1be750a263ab4cd418fb4de0568077d620a52b9e
svk31 <sigvekvalsvik@gmail.com>
2015-07-27 15:19:39 +0200

    Fix y-overflow of Dashboard

--- web/app/components/Dashboard/Dashboard.jsx
+++ web/app/components/Dashboard/Dashboard.jsx
@@ -43,7 +43,7 @@ class Dashboard extends Component {
         return (
             <div className="grid-block vertical">
                 <div className="grid-block page-layout">
-                    <div className="grid-block medium-12" style={{alignItems: "flex-start"}}>
+                    <div className="grid-block medium-12" style={{alignItems: "flex-start", overflowY: "auto", zIndex: 1}}>
                         <div className="grid-block small-up-1 medium-up-2 large-up-3">
                             {itemRows}
                         </div>
