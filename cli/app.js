61dddf9224956cbaa3f3a425327c50c288328c88
James Calfee <james@jcalfee.info>
2015-07-27 09:04:31 -0500

    Added some data-layer objects to the command-line-interface.

--- cli/app.js
+++ cli/app.js
@@ -9,7 +9,6 @@ var DebugApi = require('../dl/src/rpc_api/DebugApi');
 
 var iDB = require("../dl/src/idb-instance");
 var fakeIndexedDB = require('fake-indexeddb');
-iDB.init_instance(fakeIndexedDB);
 
 var repl_instance = repl.start({
     prompt: "Graphene > ",
@@ -25,18 +24,28 @@ repl_instance.on("exit", function () {
 var hist_file = process.env.HOME + "/.graphene_history";
 repl_history(repl_instance, hist_file);
 
-repl_instance.context.debug = new DebugApi();
+var dl_cli_index = require('dl_cli_index')
+for(let object_name in dl_cli_index) {
+    repl_instance.context[object_name] = dl_cli_index[object_name]
+}
 
-Apis.instance().init_promise.then(() => {
-    var database_api = Apis.instance().db_api();
-    var network_api = Apis.instance().network_api();
-    repl_instance.context.$g = {}
-    repl_instance.context.$g.db = database_api;
-    repl_instance.context.$g.net = network_api;
-    repl_instance.context.$g.app = new ApplicationApi();
-    repl_instance.context.$g.wallet = new WalletApi();
+repl_instance.context.$g = {}
+//var init_promise = iDB.init_instance.bind(repl_instance.context, fakeIndexedDB).init_promise
+var init_promise = iDB.init_instance(fakeIndexedDB).init_promise
+
+init_promise.then(()=> {
+    return  Apis.instance().init_promise.then(() => {
+        var database_api = Apis.instance().db_api();
+        var network_api = Apis.instance().network_api();
+        repl_instance.context.$g.db = database_api;
+        repl_instance.context.$g.net = network_api;
+        repl_instance.context.$g.app = new ApplicationApi();
+        repl_instance.context.$g.wallet = new WalletApi();
+        repl_instance.context.$g.debug = new DebugApi();
+    })
 }).catch(error => {
     console.log("[App.js] ----- ERROR ----->", error, error.stack);
+}).then(()=> {
     this.setState({loading: false});
 });
 
---
+++ dl/src/dl_cli_index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PrivateKey: require('ecc/key_private'),
+    PublicKey: require('ecc/key_public'),
+    Aes: require('ecc/aes'),
+    WalletDb: require('stores/WalletDb'),
+    AccountStore: require('stores/AccountStore')
+}
