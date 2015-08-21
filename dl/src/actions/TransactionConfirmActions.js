53538b8dc9718cb791cc948d84c038dd8655a655
James Calfee <james@jcalfee.info>
2015-08-21 11:17:28 -0500

    Deferr signing until broadcast. Allows for quick preview and serialization of the transaction #167

--- dl/src/actions/TransactionConfirmActions.js
+++ dl/src/actions/TransactionConfirmActions.js
@@ -7,14 +7,17 @@ class TransactionConfirmActions {
     }
 
     broadcast(transaction) {
-        transaction.broadcast().then( (res)=> {
-            this.actions.broadcasted(res);
-        }).catch( error => {
-            console.log("TransactionConfirmActions.broadcast error", error);
-            let message = error.message.split( '\n' )[1];
-            this.actions.error(message);
-        });
         this.dispatch();
+        setTimeout(()=>{//timeout necessary to see the UI loading indicator
+            transaction.broadcast().then( (res)=> {
+                this.actions.broadcasted(res);
+            }).catch( error => {
+                console.log("TransactionConfirmActions.broadcast error", error);
+                let message = error.message.split( '\n' )[1];
+                this.actions.error(message);
+            });
+        }, 250)
+        
     }
 
     broadcasted(res){
