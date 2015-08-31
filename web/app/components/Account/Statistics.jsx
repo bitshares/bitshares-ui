c028e37031e3c973852746210314ded0698c7b1c
valzav <i@valzav.com>
2015-08-31 15:14:53 -0400

    refactored AccountMemberStats

---
+++ web/app/components/Account/Statistics.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Translate from "react-translate-component";
+import FormattedAsset from "../Utility/FormattedAsset";
+import ChainTypes from "../Utility/ChainTypes";
+import BindToChainState from "../Utility/BindToChainState";
+
+@BindToChainState()
+class Statistics extends React.Component {
+
+    static propTypes = {
+        stat_object: ChainTypes.ChainObject.isRequired
+    }
+
+    render() {
+        let stat_object = this.props.stat_object.toJS();
+        console.log("-- Statistics.render -->", stat_object);
+        return (
+            <table className="table striped">
+                <tr>
+                    <td><Translate content="account.member.fees_paid"/> </td>
+                    <td><FormattedAsset amount={parseFloat(stat_object.lifetime_fees_paid)} asset="1.3.0" /></td>
+                </tr>
+                <tr>
+                    <td><Translate content="account.member.fees_pending"/> </td>
+                    <td><FormattedAsset amount={parseFloat(stat_object.pending_fees)} asset="1.3.0" /></td>
+                </tr>
+                <tr>
+                    <td><Translate content="account.member.fees_vested"/> </td>
+                    <td><FormattedAsset amount={parseFloat(stat_object.pending_vested_fees)} asset="1.3.0" /></td>
+                </tr>
+                <tr>
+                    <td><Translate content="account.member.in_orders"/> </td>
+                    <td><FormattedAsset amount={parseFloat(stat_object.total_core_in_orders)} asset="1.3.0" /></td>
+                </tr>
+            </table>
+        );
+    }
+}
+
+export default Statistics;
