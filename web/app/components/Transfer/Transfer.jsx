f346584e5168820fc1e8731628a11734fe567ea9
valzav <i@valzav.com>
2015-09-02 15:52:59 -0400

    Transfer works now but needs some more polish

--- web/app/components/Transfer/Transfer.jsx
+++ web/app/components/Transfer/Transfer.jsx
@@ -23,115 +23,139 @@ import ChainStore from "api/ChainStore";
 import AccountStore from "stores/AccountStore.js"
 import Wallet from "components/Wallet/Wallet";
 import validation from "common/validation";
-import AmountSelector from "../Utility/AmountSelector"
+import AmountSelector from "../Utility/AmountSelector";
+import BindToChainState from "../Utility/BindToChainState";
 
 class Transfer extends React.Component {
 
     constructor(props) {
-       super(props)
-          this.state = { 
-            from_name:"", 
-            to_name:"",
-            from_account: null
-          }
+        super(props)
+        this.state = Transfer.getInitialState();
     }
 
-    fromChanged( from_name )
-    {
-       this.setState( {from_name} )
+    static getInitialState(){
+        return {
+            from_name: "",
+            to_name: "",
+            from_account: null,
+            to_account: null,
+            amount: null,
+            asset: null,
+            errors: {
+                from: null,
+                amount: null,
+                to: null,
+                memo: null
+            }
+        };
     }
 
-    toChanged( to_name )
-    {
-       this.setState( {to_name} )
+    fromChanged(from_name) {
+        this.setState({from_name})
     }
 
-    onFromAccountChanged( from_account )
-    {
-       this.setState( {from_account} )
+    toChanged(to_name) {
+        this.setState({to_name})
     }
 
-    onAmountChanged( amount ) 
-    {
-       this.setState( {amount} )
+    onFromAccountChanged(from_account) {
+        this.setState({from_account})
+    }
+
+    onToAccountChanged(to_account) {
+        this.setState({to_account})
+    }
+
+    onAmountChanged({amount, asset}) {
+        this.setState({amount, asset})
     }
 
 
     onSubmit(e) {
         e.preventDefault();
-        this.validateTransferFields(this.state);
-        if(this.state.isValid) {
-            this.onConfirm();
-        } else {
-            this.setState({errors: this.state.errors});
-        }
+        let asset = this.state.asset;
+        let precision = utils.get_asset_precision(asset.get("precision"));
+        let amount = this.state.amount.replace( /,/g, "" )
+        AccountActions.transfer(
+            this.state.from_account.get("id"),
+            this.state.to_account.get("id"),
+            parseInt(amount * precision, 10),
+            asset.get("id"),
+            this.state.memo
+        ).then( () => {
+            this.setState(Transfer.getInitialState());
+        }).catch( e => {
+            console.log( "error: ",e)
+        } );
     }
 
     render() {
-       ChainStore.getAccount( this.state.from_name )
+        let asset_types = [];
+        let balance = null;
+        if (this.state.from_account) {
+            let account_balances = this.state.from_account.get("balances").toJS();
+            asset_types = Object.keys(account_balances);
+            if (asset_types.length > 0) {
+                let current_asset_id = this.state.asset ? this.state.asset.get("id") : asset_types[0];
+                balance = <BalanceComponent balance={account_balances[current_asset_id]}/>
+            }
+        }
+        let submitButtonClass = "button";
+        if(!this.state.from_account || !this.state.to_account || !this.state.amount || !this.state.asset)
+            submitButtonClass += " disabled";
 
-       let asset_types = []
-       if( this.state.from_account ) {
-
-       }
-
-       console.log( "FROM NAME: ", this.state.from_name )
-       console.log( "TO NAME: ", this.state.to_name )
         return (
-            <form className="grid-block vertical full-width-content" onSubmit={this.onSubmit} noValidate>
-               <div className="grid-container" style={{paddingTop: "2rem"}}>
+            <form className="grid-block vertical full-width-content" onSubmit={this.onSubmit.bind(this)} noValidate>
+                <div className="grid-container" style={{paddingTop: "2rem"}}>
                     {/*  F R O M  */}
-                    <AccountSelector label="transfer.from" 
-                                     accountName={this.state.from_name} 
+                    <AccountSelector label="transfer.from"
+                                     accountName={this.state.from_name}
                                      onChange={this.fromChanged.bind(this)}
                                      onAccountChanged={this.onFromAccountChanged.bind(this)}
                                      account={this.state.from_name}
-                    />
+                        />
+
                     <p/>
                     {/*  T O  */}
-                    <AccountSelector label="transfer.to" 
-                                     accountName={this.state.to_name} 
-                                     onChange={this.toChanged.bind(this)} 
+                    <AccountSelector label="transfer.to"
+                                     accountName={this.state.to_name}
+                                     onChange={this.toChanged.bind(this)}
+                                     onAccountChanged={this.onToAccountChanged.bind(this)}
                                      account={this.state.to_name}
-                    
-                    />
+
+                        />
                     {/*  A M O U N T   */}
                     <br/>
                     <AmountSelector label="transfer.amount"
-                                     amount={this.state.amount}
-                                     onChange={this.onAmountChanged.bind(this)}
-                                     asset={"1.3.0"}
-                                     assets={["1.3.0","1.3.1","1.3.2"]} 
-                    />
-
+                                    amount={this.state.amount}
+                                    onChange={this.onAmountChanged.bind(this)}
+                                    asset={asset_types.length > 0 ? asset_types[0] : null}
+                                    assets={asset_types}
+                                    display_balance={balance}/>
                     {/*  M E M O  */}
-
-               </div>
-
-               <div className="grid-block page-layout transfer-bottom small-horizontal">
-                    {/*  F I N A L  B A L A N C E  A N D  F E E  */}
-                    <div className="grid-block medium-3 medium-order-4 small-order-2">
-                        <div className="grid-content">
-                            {/*finalBalances*/}
-                        </div>
+                    <br/><br/>
+                    <div className={classNames({"has-error": this.state.errors.memo})}>
+                        <label>
+                            <Translate component="span" content="transfer.memo"/>
+                        </label>
+                        <textarea id="memo" rows="1" ref="memo" value={this.state.memo} tabIndex="4"/>
+                        <div>{this.state.errors.memo}</div>
                     </div>
-               </div>
+
+                    {/*  S E N D  B U T T O N  */}
+                    <div>
+                        <button className={submitButtonClass} type="submit" value="Submit" tabIndex="5">
+                            <Translate component="span" content="transfer.send" />
+                        </button>
+                    </div>
+
+                    {/* TODO: show remaining balance */}
+
+                </div>
 
             </form>
         );
     }
 }
-
-Transfer.defaultProps = {
-    cachedAccounts: {},
-    assets: {}
-};
-
-Transfer.propTypes = {
-    cachedAccounts: PropTypes.object.isRequired,
-    assets: PropTypes.object.isRequired
-};
-
-Transfer.contextTypes = { router: React.PropTypes.func.isRequired };
 
 export default Transfer;
