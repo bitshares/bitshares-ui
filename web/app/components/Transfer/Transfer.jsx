6c0449df586fb48c98361cb8e6814d4eda10406a
valzav <i@valzav.com>
2015-09-02 18:24:20 -0400

    added more checks to transfer page

--- web/app/components/Transfer/Transfer.jsx
+++ web/app/components/Transfer/Transfer.jsx
@@ -1,30 +1,13 @@
 import React from "react";
-import {PropTypes} from "react";
-import BaseComponent from "../BaseComponent";
-import FormattedAsset from "../Utility/FormattedAsset";
 import BalanceComponent from "../Utility/BalanceComponent";
-import DoneScreen from "./DoneScreen";
-import classNames from "classnames";
-import utils from "common/utils";
 import AccountActions from "actions/AccountActions";
-import AccountImage from "../Account/AccountImage";
-import AccountInfo from "../Account/AccountInfo";
 import Translate from "react-translate-component";
-import counterpart from "counterpart";
-import AutocompleteInput from "../Forms/AutocompleteInput";
-import Trigger from "react-foundation-apps/src/trigger";
-import Modal from "react-foundation-apps/src/modal";
-import ZfApi from "react-foundation-apps/src/utils/foundation-api";
-import notify from "actions/NotificationActions";
 import AccountSelector from "../Account/AccountSelector";
-import {debounce} from "lodash";
-import Immutable from "immutable";
-import ChainStore from "api/ChainStore";
 import AccountStore from "stores/AccountStore";
-import Wallet from "components/Wallet/Wallet";
-import validation from "common/validation";
 import AmountSelector from "../Utility/AmountSelector";
-import BindToChainState from "../Utility/BindToChainState";
+import utils from "common/utils";
+
+// TODO: add support for url params: from, to, amount, asset, memo
 
 class Transfer extends React.Component {
 
@@ -39,14 +22,9 @@ class Transfer extends React.Component {
             to_name: "",
             from_account: null,
             to_account: null,
-            amount: null,
+            amount: "",
             asset: null,
-            errors: {
-                from: null,
-                amount: null,
-                to: null,
-                memo: null
-            }
+            memo: ""
         };
     }
 
@@ -70,6 +48,9 @@ class Transfer extends React.Component {
         this.setState({amount, asset})
     }
 
+    onMemoChanged(e) {
+        this.setState({memo: e.target.value});
+    }
 
     onSubmit(e) {
         e.preventDefault();
@@ -90,9 +71,13 @@ class Transfer extends React.Component {
     }
 
     render() {
+        let from_error = null;
+        if(this.state.from_account && !AccountStore.getState().linkedAccounts.contains(this.state.from_name) )
+            from_error = "Not your account";
+
         let asset_types = [];
         let balance = null;
-        if (this.state.from_account) {
+        if (this.state.from_account && !from_error) {
             let account_balances = this.state.from_account.get("balances").toJS();
             asset_types = Object.keys(account_balances);
             if (asset_types.length > 0) {
@@ -100,54 +85,48 @@ class Transfer extends React.Component {
                 balance = (<span>Available: <BalanceComponent balance={account_balances[current_asset_id]}/></span>)
             }
         }
+
         let submitButtonClass = "button";
-        if(!this.state.from_account || !this.state.to_account || !this.state.amount || !this.state.asset)
+        if(!this.state.from_account || !this.state.to_account || !this.state.amount || !this.state.asset || from_error)
             submitButtonClass += " disabled";
-
-        let from_error = null
-
-        let from = ChainStore.getAccount( this.state.from_name )
-        if( from && !AccountStore.getState().linkedAccounts.contains( this.state.from_name ) ) {
-           from_error = "Not your Account"
-        }
 
         return (
             <form className="grid-block vertical full-width-content" onSubmit={this.onSubmit.bind(this)} noValidate>
-                <div className="grid-container" style={{paddingTop: "2rem"}}>
+                <div className="grid-container large-5 medium-7 small-11" style={{paddingTop: "2rem"}}>
                     {/*  F R O M  */}
-                    <AccountSelector label="transfer.from"
-                                     error={from_error}
-                                     accountName={this.state.from_name}
-                                     onChange={this.fromChanged.bind(this)}
-                                     onAccountChanged={this.onFromAccountChanged.bind(this)}
-                                     account={this.state.from_name}
-                        />
-
-                    <p/>
+                    <div className="content-block">
+                        <AccountSelector label="transfer.from"
+                                         accountName={this.state.from_name}
+                                         onChange={this.fromChanged.bind(this)}
+                                         onAccountChanged={this.onFromAccountChanged.bind(this)}
+                                         account={this.state.from_name}
+                                         error={from_error}
+                                         tabIndex={1}/>
+                    </div>
                     {/*  T O  */}
-                    <AccountSelector label="transfer.to"
-                                     accountName={this.state.to_name}
-                                     onChange={this.toChanged.bind(this)}
-                                     onAccountChanged={this.onToAccountChanged.bind(this)}
-                                     account={this.state.to_name}
-
-                        />
+                    <div className="content-block">
+                        <AccountSelector label="transfer.to"
+                                         accountName={this.state.to_name}
+                                         onChange={this.toChanged.bind(this)}
+                                         onAccountChanged={this.onToAccountChanged.bind(this)}
+                                         account={this.state.to_name}
+                                         tabIndex={2}/>
+                    </div>
                     {/*  A M O U N T   */}
-                    <br/>
-                    <AmountSelector label="transfer.amount"
-                                    amount={this.state.amount}
-                                    onChange={this.onAmountChanged.bind(this)}
-                                    asset={asset_types.length > 0 ? asset_types[0] : null}
-                                    assets={asset_types}
-                                    display_balance={balance}/>
+                    <div className="content-block">
+                        <AmountSelector label="transfer.amount"
+                                        amount={this.state.amount}
+                                        onChange={this.onAmountChanged.bind(this)}
+                                        asset={asset_types.length > 0 ? asset_types[0] : null}
+                                        assets={asset_types}
+                                        display_balance={balance}
+                                        tabIndex={3}/>
+                    </div>
                     {/*  M E M O  */}
-                    <br/><br/>
-                    <div className={classNames({"has-error": this.state.errors.memo})}>
-                        <label>
-                            <Translate component="span" content="transfer.memo"/>
-                        </label>
-                        <textarea id="memo" rows="1" ref="memo" value={this.state.memo} tabIndex="4"/>
-                        <div>{this.state.errors.memo}</div>
+                    <div className="content-block">
+                        <label><Translate component="span" content="transfer.memo"/></label>
+                        <textarea rows="1" value={this.state.memo} tabIndex="4" onChange={this.onMemoChanged.bind(this)}/>
+                        {/*<div>{memo_error}</div>*/}
                     </div>
 
                     {/*  S E N D  B U T T O N  */}
