365ae0d38494b0b9b54d5772e6d97876a77bcb13
James Calfee <james@jcalfee.info>
2015-09-16 15:19:43 -0500

    Cleaned up syntax

--- web/app/components/Utility/ChainResolveComponents.jsx
+++ web/app/components/Utility/ChainResolveComponents.jsx
@@ -1,27 +1,24 @@
 /** Generic set of components for dealing with data in the ChainStore */
 import React, {Component, Children} from "react"
-import AltContainer from "alt/AltContainer"
+import connectToStores from "alt/utils/connectToStores";
 import ChainTypes from "components/Utility/ChainTypes"
 import BindToChainState from "components/Utility/BindToChainState"
 import Immutable from "immutable"
 
 import AccountStore from "stores/AccountStore"
 
+@connectToStores
 export class ResolveLinkedAccounts extends Component {
+    static getStores() {
+        return [AccountStore]
+    }
+    static getPropsFromStores() {
+        return AccountStore.getState()
+    }
     render() {
-        return (
-            <AltContainer
-                stores={[AccountStore]}
-                inject={{
-                    resolvedLinkedAccounts: () => {
-                        var l = Immutable.List(AccountStore.getState().linkedAccounts)
-                        console.log("... Immutable.List.isList(l)", Immutable.List.isList(l))
-                        return Immutable.List(AccountStore.getState().linkedAccounts)
-                    }
-                }}>
-                <ResolveLinkedAccountsChainState children={this.props.children}/>
-            </AltContainer>
-        )
+        return <ResolveLinkedAccountsChainState
+            linkedAccounts={this.props.linkedAccounts}
+            children={this.props.children}/>
     }
 }
 
@@ -29,20 +26,20 @@ export class ResolveLinkedAccounts extends Component {
 class ResolveLinkedAccountsChainState extends Component {
     
     static propTypes = {
-        resolvedLinkedAccounts: ChainTypes.ChainAccountsList.isRequired
+        linkedAccounts: ChainTypes.ChainAccountsList.isRequired
     }
 
     render() {
-        var resolvedLinkedAccounts = []
-        this.props.resolvedLinkedAccounts.forEach( account => {
+        var linkedAccounts = []
+        this.props.linkedAccounts.forEach( account => {
             if( ! account) return
             console.log("... account.toJS()", account.toJS())
-            resolvedLinkedAccounts.push(account)
+            linkedAccounts.push(account)
         })
         var child = Children.only(this.props.children)
-        if( ! child) return <span>{resolvedLinkedAccounts}</span>
+        if( ! child) return <span>{linkedAccounts.map(a => <br>{a.toJS()}</br>)}</span>
         // Pass the list to a child reactjs component as this.props.resolvedLinkedAccounts
-        child = React.addons.cloneWithProps(child, { resolvedLinkedAccounts })
+        child = React.addons.cloneWithProps(child, { linkedAccounts })
         return <span>{child}</span>
     }
     
