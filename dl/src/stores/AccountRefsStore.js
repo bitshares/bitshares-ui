907e055015caacf7bd3c9c56d496beda4a70b15f
James Calfee <james@jcalfee.info>
2015-10-02 12:58:42 -0500

    Keep brainkey generated keys in the account references view (do not hide them like titan keys) #209

--- dl/src/stores/AccountRefsStore.js
+++ dl/src/stores/AccountRefsStore.js
@@ -25,6 +25,11 @@ class AccountRefsStore extends BaseStore {
         }
     }
     
+    onAddPrivateKey({private_key_object}) {
+        if(ChainStore.getAccountRefsOfKey(private_key_object.pubkey) !== undefined)
+            this.chainStoreUpdate()
+    }
+    
     loadDbData() {
         this.setState(this._getInitialState())
         return loadNoAccountRefs()
@@ -35,16 +40,30 @@ class AccountRefsStore extends BaseStore {
     chainStoreUpdate() {
         if(this.chainstore_account_ids_by_key === ChainStore.account_ids_by_key) return
         this.chainstore_account_ids_by_key = ChainStore.account_ids_by_key
+        this.checkPrivateKeyStore()
+    }
+    
+    checkPrivateKeyStore() {
         var no_account_refs = this.no_account_refs
         var account_refs = Immutable.Set()
         PrivateKeyStore.getState().keys.keySeq().forEach( pubkey => {
             if(no_account_refs.has(pubkey)) return
             var refs = ChainStore.getAccountRefsOfKey(pubkey)
-            if(refs === undefined) {
-                // this.setState({loading_account_refs: true})
-                return
-            }
+            if(refs === undefined) return
             if( ! refs.size) {
+                // Performance optimization... 
+                // There are no references for this public key, this is going
+                // to block it.  There many be many TITAN keys that do not have
+                // accounts for example.
+                {
+                    // Do Not block brainkey generated keys.. Those are new and
+                    // account references may be pending.
+                    var private_key_object = PrivateKeyStore.getState().keys.get(pubkey)
+                    if( typeof private_key_object.brainkey_sequence === 'number' ) {
+                        console.log("private_key_object.brainkey_sequence", private_key_object.brainkey_sequence)
+                        return
+                    }
+                }
                 no_account_refs = no_account_refs.add(pubkey)
                 return
             }
@@ -59,12 +78,6 @@ class AccountRefsStore extends BaseStore {
             this.no_account_refs = no_account_refs
             saveNoAccountRefs(no_account_refs)
         }
-        return null
-    }
-    
-    onAddPrivateKey({private_key_object}) {
-        if(ChainStore.getAccountRefsOfKey(private_key_object.pubkey) !== undefined)
-            this.chainStoreUpdate()
     }
     
 }
