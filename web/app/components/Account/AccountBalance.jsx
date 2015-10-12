8fbf4fe4a93c9fc2da2ab7d02677a5e69651c1a6
Daniel Larimer <dan@bitshares.org>
2015-10-12 11:36:04 -0400

    update deposit/withdraw

---
+++ web/app/components/Account/AccountBalance.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import FormattedAsset from "../Utility/FormattedAsset";
+import ChainTypes from "../Utility/ChainTypes";
+import BindToChainState from "../Utility/BindToChainState";
+import BalanceComponent from "../Utility/BalanceComponent";
+
+/**
+ *  Given a balance_object, displays it in a pretty way
+ *
+ *  Expects one property, 'balance' which should be a balance_object id
+ */
+
+@BindToChainState({keep_updating: true})
+class AccountBalance extends React.Component {
+
+    static propTypes = {
+      account: ChainTypes.ChainAccount.isRequired,
+      asset: ChainTypes.ChainAsset.isRequired
+    }
+
+    render() {
+        let asset_id = this.props.asset.get('id');
+        let balance_id = this.props.account.getIn( ['balances',asset_id] );
+
+        if( balance_id ) return <BalanceComponent balance={balance_id} />
+        else return <span>0&nbsp;{this.props.asset.get('symbol')}</span>
+    }
+}
+
+export default AccountBalance;
