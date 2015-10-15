123884ce580e43cf85036dbdd41c0de18b18b301
valzav <i@valzav.com>
2015-10-14 21:20:27 -0400

    restore prev. electron package name to make it compatible with initial version

--- dl/src/stores/SettingsStore.js
+++ dl/src/stores/SettingsStore.js
@@ -44,6 +44,7 @@ class SettingsStore {
                 "tr"
             ],
             connection: [
+                "wss://bitshares.openledger.info/ws",
                 "ws://127.0.0.1:8090"
             ]
             // confirmMarketOrder: [
