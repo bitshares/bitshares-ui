a99ca8505f789640283978061a02228b5db9c160
James Calfee <james@jcalfee.info>
2015-10-15 11:17:58 -0500

    Private key viewer used in account permissions page (close #340)

---
+++ web/app/components/PrivateKeyView.jsx
@@ -0,0 +1,98 @@
+import React, {Component} from "react"
+import Modal from "react-foundation-apps/src/modal"
+import ZfApi from "react-foundation-apps/src/utils/foundation-api"
+import WalletUnlockActions from "actions/WalletUnlockActions"
+import WalletDb from "stores/WalletDb"
+
+export default class PrivateKeyView extends Component {
+    
+    static propTypes = {
+        pubkey: React.PropTypes.string.isRequired
+    }
+    
+    constructor() {
+        super()
+        this.state = this._getInitialState()
+    }
+    
+    _getInitialState() {
+        return { wif: null }
+    }
+    
+    reset() {
+        this.setState(this._getInitialState())
+    }
+    
+    componentDidMount() {
+        var modalId = "key_view_modal" + this.props.pubkey
+        let modal = React.findDOMNode(this.refs[modalId])
+        ZfApi.subscribe(modalId, (name, msg) => {
+            //DEBUG 
+            console.log('... name, msg',name, msg)
+            if(name !== modalId) return
+            if(msg === "close") this.reset()
+        })
+    }
+    
+    render() {
+        var modalId = "key_view_modal" + this.props.pubkey
+        var has_private = PrivateKeyStore.getState().keys.has(this.props.pubkey)
+        if( ! has_private) return <span>{this.props.children}</span>
+        return <span>
+            <a onClick={this.onOpen.bind(this)}>{this.props.children}</a>
+            <Modal ref={modalId} id={modalId} overlay={true} overlayClose={false}>
+                <a onClick={this.onClose.bind(this)} className="close-button">&times;</a>
+                <h3>Private Key View</h3>
+                <div className="grid-block vertical">
+                    <div className="content-block">
+                    
+                        <div className="grid-content">
+                            <label>Public Key</label>
+                            {this.props.pubkey}
+                        </div>
+                        <br/>
+                        
+                        <div className="grid-block grid-content">
+                            <label>Private Key (WIF - Wallet Import Format)</label>
+                            <div>
+                                {this.state.wif ? <span>
+                                    {this.state.wif}
+                                    &nbsp;(<a onClick={this.onHide.bind(this)}>hide</a>)
+                                </span>:<span>
+                                    (<a onClick={this.onShow.bind(this)}>show</a>)
+                                </span>}
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="button-group">
+                    <a onClick={this.onClose.bind(this)} className="secondary button">close</a>
+                </div>
+            </Modal>
+        </span>
+    }
+    
+    onOpen() {
+        var modalId = "key_view_modal" + this.props.pubkey
+        ZfApi.publish(modalId, "open")
+    }
+    
+    onClose() {
+        this.reset()
+        var modalId = "key_view_modal" + this.props.pubkey
+        ZfApi.publish(modalId, "close")
+    }
+    
+    onShow() {
+        WalletUnlockActions.unlock().then( () => {
+            var private_key = WalletDb.getPrivateKey(this.props.pubkey)
+            this.setState({ wif: private_key.toWif() })
+        })
+    }
+    
+    onHide() {
+        this.setState({ wif: null })
+    }
+    
+}
