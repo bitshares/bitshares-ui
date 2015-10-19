634ea054cd46430915f010e6d3461b6bbcc9843d
James Calfee <james@jcalfee.info>
2015-10-19 16:31:10 -0500

    Sample alt action debug command

--- dl/src/alt-instance.js
+++ dl/src/alt-instance.js
@@ -1,4 +1,9 @@
 var Alt = require("alt");
 var alt = new Alt();
 
+
+// DEBUG log all action events
+// https://github.com/goatslacker/alt/issues/516
+// alt.dispatcher.register('alt.dispatcher', console.log.bind(console))
+
 module.exports = alt;
