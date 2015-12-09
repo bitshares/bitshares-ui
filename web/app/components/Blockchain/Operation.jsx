c81b68d673e77ce2c673b13ab188cfaa4cc7db44
valzav <i@valzav.com>
2015-12-09 18:38:07 -0500

    add operation help popover on recent transactions list

--- web/app/components/Blockchain/Operation.jsx
+++ web/app/components/Blockchain/Operation.jsx
@@ -18,6 +18,7 @@ import BindToChainState from "../Utility/BindToChainState";
 import FormattedPrice from "../Utility/FormattedPrice";
 import ChainTypes from "../Utility/ChainTypes";
 import ChainStore from "api/ChainStore";
+import Popover from "react-popover";
 
 require("./operations.scss");
 
@@ -57,12 +58,24 @@ class Row extends React.Component {
 
     constructor(props) {
         super(props);
-        this.showDetails = this.showDetails.bind(this);
+        this.state = {detailsPopoverOpen: false};
+        //this.showDetails = this.showDetails.bind(this);
+        this.togglePopover = this.togglePopover.bind(this);
+        this.closePopover = this.closePopover.bind(this);
     }
 
-    showDetails(e) {
+    //showDetails(e) {
+    //    e.preventDefault();
+    //    //this.context.history.pushState(null, `/block/${this.props.block}`);
+    //}
+
+    togglePopover(e) {
         e.preventDefault();
-        this.context.history.pushState(null, `/block/${this.props.block}`);
+        this.setState({detailsPopoverOpen: !this.state.detailsPopoverOpen});
+    }
+
+    closePopover() {
+        this.setState({detailsPopoverOpen: false});
     }
 
     render() {
@@ -74,9 +87,19 @@ class Row extends React.Component {
            pending = <span>(<Translate content="operation.pending" blocks={block - last_irreversible_block_num} />)</span>
 
         fee.amount = parseInt(fee.amount, 10);
+
+
+        const details_popover_body = <div>
+            TODO: Operation details here
+        </div>;
+
         return (
                 <tr key={key}>
-                    {hideOpLabel ? null : <td className="left-td"><a href onClick={this.showDetails}><TransactionLabel color={color} type={type} /></a></td>}
+                    {hideOpLabel ? null : <td className="left-td">
+                        <Popover isOpen={this.state.detailsPopoverOpen} onOuterAction={this.closePopover} body={details_popover_body}>
+                            <span className="cursor-help" onClick={this.togglePopover}><TransactionLabel color={color} type={type} /></span>
+                        </Popover>
+                    </td>}
                     <td>{this.props.info}&nbsp;{pending}&nbsp;{hideFee ? null : <span className="facolor-fee">(<FormattedAsset amount={fee.amount} asset={fee.asset_id} /> fee)</span>}</td>
                     <td className="cursor-pointer" onClick={this.showDetails}><BlockTime block_number={block}/></td>
                 </tr>
@@ -825,6 +848,7 @@ class Operation extends React.Component {
                 hideFee={this.props.hideFee}
                 hideOpLabel={this.props.hideOpLabel}
                 info={column}
+                op={op}
             >
             </Row>
         ) : null;
