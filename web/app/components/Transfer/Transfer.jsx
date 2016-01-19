97615505ee2c0ab46f524f802c6e73218f736bce
svk31 <sigvekvalsvik@gmail.com>
2016-01-19 20:19:20 +0100

    Estimate create_proposal fee for proposed transfers

--- web/app/components/Transfer/Transfer.jsx
+++ web/app/components/Transfer/Transfer.jsx
@@ -162,7 +162,7 @@ class Transfer extends React.Component {
 
         // Estimate fee
         let globalObject = ChainStore.getObject("2.0.0");
-        let fee = utils.estimateFee("transfer", null, globalObject);
+        let fee = utils.estimateFee(propose ? "proposal_create" : "transfer", null, globalObject);
         if (from_account && !from_error) {
             let account_balances = from_account.get("balances").toJS();
             asset_types = Object.keys(account_balances).sort(utils.sortID);
