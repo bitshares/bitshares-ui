981e22d9e0c5f904b9b1f94e9b4695d549777fe3
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-01-28 13:38:48 +0100

    Move ProposedTransactions above RecentTransactions in AccountOverview

--- web/app/components/Account/AccountOverview.jsx
+++ web/app/components/Account/AccountOverview.jsx
@@ -197,16 +197,18 @@ class AccountOverview extends React.Component {
                         </tbody>
                     </table>
                 </div> : null}
+                {account.get("proposals").length ? 
+                <div className="content-block">
+                    <h3><Translate content="explorer.proposals.title" account={account.get("id")} /></h3>
+                    <Proposals accountList={Immutable.fromJS([account.get("id")])}/>
+                </div> : null}
+
                 <div className="content-block">
                     <RecentTransactions
                         accountsList={Immutable.fromJS([account.get("id")])}
                         compactView={false}
                         showMore={true}
                     />
-                </div>
-                <div className="content-block">
-                    <h3><Translate content="explorer.proposals.title" account={account.get("id")} /></h3>
-                    <Proposals accountList={Immutable.fromJS([account.get("id")])}/>
                 </div>
             </div>
 
