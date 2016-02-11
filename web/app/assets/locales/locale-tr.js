36de0cfa666d40134cc3f2a2ae54f6a61f6c3066
bitsacer <bitsacer@gmail.com>
2016-02-11 15:35:38 +0200

    Update locale-tr.js

--- web/app/assets/locales/locale-tr.js
+++ web/app/assets/locales/locale-tr.js
@@ -147,7 +147,9 @@
             force_settlement_delay_sec: "Zorunlu hesaplaşma gecikmesi (dakika)",
             force_settlement_offset_percent: "Zorunlu hesaplaşmanın yüzdelik sapması",
             maximum_force_settlement_volume: "Max zorunlu mahsup hacmi (yüzde)",
-            backing: "Kısanın dayandığı aktif"
+            backing: "Kısanın dayandığı aktif",
+            error_precision: "Aktif %(asset)s ile aynı hassasiyete sahip değil",
+            error_invalid: "O aktif kullanılamaz"
         },
         connections: {
             known: "Tarafından Tanınıyor",
@@ -813,6 +815,12 @@
         title: "%(asset)s ödenmesini talep et",
         amount: "Ödenecek miktar",
         submit: "Aktif tasfiye et"
+      },
+      reserve: {
+        title: "Rezerv (yakılan) aktif",
+        amount: "Rezerv edilecek miktar",
+        from: "Rezerv yapılacak hesap",
+        submit: "Rezerv"
       },
       ok: "Tamam"
     },
