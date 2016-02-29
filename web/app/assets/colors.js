88e2f6e0822336ab161174ca352e3019e1a1981f
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-02-29 12:38:23 +0100

    Add rgba fill colors for depth charts

--- web/app/assets/colors.js
+++ web/app/assets/colors.js
@@ -1,8 +1,12 @@
 module.exports = {
     olDarkTheme: {
         bidColor: "#50D2C2",
+        bidFillColor: "rgba(80, 210, 194, 0.5)",
         askColor: "#E3745B",
+        askFillColor: "rgba(227, 116, 91, 0.5)",
         callColor: "#BBBF2B",
+        settleColor: "rgba(125, 134, 214, 1)",
+        settleFillColor: "rgba(125, 134, 214, 0.5)",
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#e0e0e0",
@@ -10,8 +14,12 @@ module.exports = {
     },
     darkTheme: {
         bidColor: "#50D2C2",
+        bidFillColor: "rgba(80, 210, 194, 0.5)",
         askColor: "#E3745B",
+        askFillColor: "rgba(227, 116, 91, 0.5)",
         callColor: "#BBBF2B",
+        settleColor: "rgba(125, 134, 214, 1)",
+        settleFillColor: "rgba(125, 134, 214, 0.5)",
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#e0e0e0",
@@ -19,8 +27,12 @@ module.exports = {
     },
     lightTheme: {
         bidColor: "#258A14",
+        bidFillColor: "rgba(80, 210, 194, 0.5)",
         askColor: "#EA340B",
+        askFillColor: "rgba(227, 116, 91, 0.5)",
         callColor: "#BBBF2B",
+        settleColor: "rgba(125, 134, 214, 1)",
+        settleFillColor: "rgba(125, 134, 214, 0.5)",
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#3F2E55",
