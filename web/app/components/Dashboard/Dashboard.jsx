0940c508f33ff2247b9366f5f360d73ad85e25b3
svk31 <sigvekvalsvik@gmail.com>
2016-05-24 21:17:01 +0200

    Add some padding to Dashboard sides

--- web/app/components/Dashboard/Dashboard.jsx
+++ web/app/components/Dashboard/Dashboard.jsx
@@ -118,7 +118,7 @@ class Dashboard extends React.Component {
 
         return (
             <div ref="wrapper" className="grid-block page-layout vertical">
-                <div ref="container" className="grid-container" style={{paddingTop: 25}}>
+                <div ref="container" className="grid-container" style={{padding: "25px 10px 0 10px"}}>
                     <Translate content="exchange.featured" component="h4" />
                     <div className="grid-block small-up-1 medium-up-3 large-up-4 no-overflow">
                         {markets}
