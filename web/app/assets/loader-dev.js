762d9bb29495057bf32b844c231f7b02bd8431fb
svk31 <sigvekvalsvik@gmail.com>
2016-06-21 21:36:13 +0200

    Fix missing json dictionary in assets

--- web/app/assets/loader-dev.js
+++ web/app/assets/loader-dev.js
@@ -1,6 +1,7 @@
 require("./stylesheets/app.scss");
 require("file?name=index.html!./index-dev.html");
 require("file?name=favicon.ico!./favicon.ico");
+require("file?name=dictionary.json!common/dictionary_en.json");
 require("babel/polyfill");
 require("whatwg-fetch");
 require("indexeddbshim");
