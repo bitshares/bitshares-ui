fc1d6a38740d4ef174b38e0cf9488c55bea4839b
svk31 <sigvekvalsvik@gmail.com>
2016-06-23 21:07:25 +0200

    Add Gold and Silver to default assets

--- dl/src/stores/SettingsStore.js
+++ dl/src/stores/SettingsStore.js
@@ -29,7 +29,7 @@ class SettingsStore {
         let topMarkets = [
             "MKR", "OPEN.MKR", "BTS", "OPEN.ETH", "ICOO", "BTC", "OPEN.LISK",
             "OPEN.STEEM", "OPEN.DAO", "PEERPLAYS", "USD", "CNY", "BTSR", "OBITS",
-            "OPEN.DGD", "EUR", "TRADE.BTC", "CASH.BTC"
+            "OPEN.DGD", "EUR", "TRADE.BTC", "CASH.BTC", "GOLD", "SILVER"
         ];
 
         this.preferredBases = Immutable.List(["OPEN.BTC", "USD", CORE_ASSET, "BTC", "CNY"]);
