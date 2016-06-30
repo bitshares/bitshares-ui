132fefecef27d6b527dd82fca0ed5981ab9293cb
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-06-30 20:29:38 +0200

    Properly fix api error redirection

--- web/app/App.jsx
+++ web/app/App.jsx
@@ -99,8 +99,6 @@ class App extends React.Component {
             NotificationStore.listen(this._onNotificationChange.bind(this));
             SettingsStore.listen(this._onSettingsChange.bind(this));
 
-
-
             ChainStore.init().then(() => {
                 this.setState({synced: true});
 
@@ -110,15 +108,15 @@ class App extends React.Component {
                     AccountStore.tryToSetCurrentAccount();
                     this.setState({loading: false});
                 }).catch(error => {
-                    console.log("[App.jsx] ----- ERROR ----->", error, error.stack);
+                    console.log("[App.jsx] ----- ERROR ----->", error);
                     this.setState({loading: false});
                 });
             }).catch(error => {
-                console.log("[App.jsx] ----- ChainStore.init error ----->", error, error.stack);
+                console.log("[App.jsx] ----- ChainStore.init error ----->", error);
                 this.setState({loading: false});
             });
         } catch(e) {
-            console.error(e);
+            console.error("e:", e);
         }
         const user_agent = navigator.userAgent.toLowerCase();
         if (!(window.electron || user_agent.indexOf("firefox") > -1 || user_agent.indexOf("chrome") > -1 || user_agent.indexOf("edge") > -1)) {
@@ -262,20 +260,28 @@ let willTransitionTo = (nextState, replaceState, callback) => {
 
     if (nextState.location.pathname === "/init-error") {
 
-        var db = iDB.init_instance(window.openDatabase ? (shimIndexedDB || indexedDB) : indexedDB).init_promise;
-        return db.then(() => {
-            Apis.reset("ws://127.0.0.1:8090").init_promise
-            .then(() => {
+        return Apis.reset(connectionString).init_promise
+        .then(() => {
+            var db = iDB.init_instance(window.openDatabase ? (shimIndexedDB || indexedDB) : indexedDB).init_promise;
+            return db.then(() => {
                 return callback();
             }).catch((err) => {
                 console.log("err:", err);
                 return callback();
             });
+        }).catch((err) => {
+            console.log("err:", err);
+            return callback();
         });
 
     }
     Apis.instance(connectionString).init_promise.then(() => {
-        var db = iDB.init_instance(window.openDatabase ? (shimIndexedDB || indexedDB) : indexedDB).init_promise;
+        var db;
+        try {
+            db = iDB.init_instance(window.openDatabase ? (shimIndexedDB || indexedDB) : indexedDB).init_promise;
+        } catch(err) {
+            console.log("db init error:", err);
+        }
         return Promise.all([db]).then(() => {
             console.log("db init done");
             return Promise.all([
@@ -346,8 +352,8 @@ let routes = (
         <Route path="asset/:symbol" component={AssetContainer}/>
         <Route path="create-account" component={CreateAccount}/>
         <Route path="existing-account" component={ExistingAccount}>
-            <IndexRoute component={ExistingAccountOptions}/>
-            <Route path="import-backup" component={BackupRestore}/>
+            <IndexRoute component={BackupRestore}/>
+            <Route path="import-backup" component={ExistingAccountOptions}/>
             <Route path="import-keys" component={ImportKeys}/>
             <Route path="brainkey" component={Brainkey}/>
             <Route path="balance-claim" component={BalanceClaimActive}/>
