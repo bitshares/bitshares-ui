aa26ae560ae950aefa5719cdc6ebb25fb9684637
svk31 <sigvekvalsvik@gmail.com>
2016-08-03 20:34:17 +0200

    Restore Settle functionality, and add it to 'action' dropdown

--- web/app/components/Blockchain/CollateralPosition.jsx
+++ web/app/components/Blockchain/CollateralPosition.jsx
@@ -99,7 +99,7 @@ class CollateralPosition extends React.Component {
                 <td>
                     <ActionSheet>
                         <ActionSheet.Button title="">
-                            <a>
+                            <a className="action-button">
                                 &nbsp;<Translate content="account.perm.action" /> &nbsp;
                                 <Icon className="icon-14px" name="chevron-down"/>
                             </a>
