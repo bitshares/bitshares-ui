282db8e2d1474e95ae761a80c8735a59245fd450
svk31 <sigvekvalsvik@gmail.com>
2016-08-13 10:33:02 +0200

    Add current price to Dashboard MarketCards

--- web/app/components/Dashboard/MarketCard.jsx
+++ web/app/components/Dashboard/MarketCard.jsx
@@ -64,7 +64,7 @@ class MarketCard extends React.Component {
         let stats = marketStats.get(marketID);
         let changeClass = !stats ? "" : parseFloat(stats.change) > 0 ? "change-up" : parseFloat(stats.change) < 0 ? "change-down" : "";
 
-        if ((base.get("issuer") === "1.2.0" || 
+        if ((base.get("issuer") === "1.2.0" ||
              base.get("issuer") === "1.2.3") &&
            !(base.get("symbol") != "GOLD" ||
              base.get("symbol") != "BTC" ||
@@ -82,6 +82,14 @@ class MarketCard extends React.Component {
                 <div className="grid-block vertical no-overflow">
                     <div className="fm-title" style={{visibility: this.props.new ? "visible" : "hidden"}}><Translate content="exchange.new" /></div>
                     <div className="fm-name">{desc.short_name ? <span>{desc.short_name}&nbsp;<span>({name})</span></span> : <AssetName name={base.get("symbol")} />}</div>
+                    <div className="fm-volume">{!stats ? null : utils.format_price(
+                        stats.close.quote.amount,
+                        base,
+                        stats.close.base.amount,
+                        quote,
+                        true,
+                        true
+                    )}</div>
                     <div className="fm-volume">{!stats ? null : utils.format_volume(stats.volumeBase, quote.get("precision"))} <AssetName name={quote.get("symbol")} /></div>
                     <div className={cnames("fm-change", changeClass)}>{!stats ? null : stats.change}%</div>
                 </div>
