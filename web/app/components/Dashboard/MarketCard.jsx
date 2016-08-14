9ca417728153902e4e4143e9d5960fd94c3b9bbc
svk31 <sigvekvalsvik@gmail.com>
2016-08-14 08:49:43 +0200

    Fix price display in MarketCard

--- web/app/components/Dashboard/MarketCard.jsx
+++ web/app/components/Dashboard/MarketCard.jsx
@@ -79,6 +79,8 @@ class MarketCard extends React.Component {
             imgName = "bts";
         }
 
+        console.log("stats:", stats);
+
         return (
             <div className={cnames("grid-block no-overflow fm-container", this.props.className)} onClick={this.goToMarket.bind(this)}>
                 <div className="grid-block vertical shrink">
@@ -87,7 +89,7 @@ class MarketCard extends React.Component {
                 <div className="grid-block vertical no-overflow">
                     <div className="fm-title" style={{visibility: this.props.new ? "visible" : "hidden"}}><Translate content="exchange.new" /></div>
                     <div className="fm-name">{desc.short_name ? <span>{desc.short_name}</span> : <AssetName name={base.get("symbol")} />}</div>
-                    <div className="fm-volume">{(!stats || !stats.close || !("amount" in stats.close)) ? null : utils.format_price(
+                    <div className="fm-volume">{(!stats || !stats.close) ? null : utils.format_price(
                         stats.close.quote.amount,
                         base,
                         stats.close.base.amount,
