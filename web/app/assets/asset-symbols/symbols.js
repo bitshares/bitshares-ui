86c8fa9935411d943d5e9d9f72fbe96134796da6
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2017-02-16 09:30:27 +0100

    Add bitSilver asset icon

--- web/app/assets/asset-symbols/symbols.js
+++ web/app/assets/asset-symbols/symbols.js
@@ -7,7 +7,7 @@ require("file-loader?name=asset-symbols/[name].png!./eur.png");
 require("file-loader?name=asset-symbols/[name].png!./cny.png");
 require("file-loader?name=asset-symbols/[name].png!./gold.png");
 require("file-loader?name=asset-symbols/[name].png!./btc.png");
-require("file-loader?name=asset-symbols/silver.png!./bts.png");
+require("file-loader?name=asset-symbols/silver.png!./silver.png");
 
 // 3rd party assets
 require("file-loader?name=asset-symbols/[name].png!./eth.png");
