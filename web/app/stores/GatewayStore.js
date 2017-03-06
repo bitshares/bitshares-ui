9c4ed5d222b2f96afddcd4c169123c56df122b9c
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2017-03-06 09:01:21 +0100

    Persist backedCoins in localStorage

--- web/app/stores/GatewayStore.js
+++ web/app/stores/GatewayStore.js
@@ -1,11 +1,15 @@
 import Immutable from "immutable";
 import alt from "alt-instance";
 import GatewayActions from "actions/GatewayActions";
+import ls from "common/localStorage";
+
+const STORAGE_KEY = "__graphene__";
+let ss = new ls(STORAGE_KEY);
 
 class GatewayStore {
     constructor() {
         this.coins = Immutable.Map();
-        this.backedCoins = Immutable.Map();
+        this.backedCoins = Immutable.Map(ss.get("backedCoins", {}));
 
         this.bindListeners({
             onFetchCoins: GatewayActions.fetchCoins
@@ -15,6 +19,8 @@ class GatewayStore {
     onFetchCoins({backer, coins, backedCoins}) {
         this.coins = this.coins.set(backer, coins);
         this.backedCoins = this.backedCoins.set(backer, backedCoins);
+
+        ss.set("backedCoins", this.backedCoins.toJS());
     }
 }
 
