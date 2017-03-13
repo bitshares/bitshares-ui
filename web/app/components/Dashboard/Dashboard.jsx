416f9bdc148ea662a56ed55c1cab466ca6f76026
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2017-03-13 10:28:22 +0100

    Fix accountpage menu margin + some minor css changes

--- web/app/components/Dashboard/Dashboard.jsx
+++ web/app/components/Dashboard/Dashboard.jsx
@@ -174,7 +174,9 @@ class Dashboard extends React.Component {
         return (
             <div ref="wrapper" className="grid-block page-layout vertical">
                 <div ref="container" className="grid-container" style={{padding: "25px 10px 0 10px"}}>
-                    <Translate content="exchange.featured" component="h4" />
+                    <div className="block-content-header" style={{marginBottom: 15}}>
+                    <Translate content="exchange.featured"/>
+                    </div>
                     <div className="grid-block small-up-1 medium-up-3 large-up-4 no-overflow fm-outer-container">
                         {markets}
                     </div>
