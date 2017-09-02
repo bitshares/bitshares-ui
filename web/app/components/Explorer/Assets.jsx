7ecc9175cb5fb8ead356899587e1d137effe1451
skenan <skenan1992@gmail.com>
2017-09-02 16:03:26 -0700

    Fix #345

--- web/app/components/Explorer/Assets.jsx
+++ web/app/components/Explorer/Assets.jsx
@@ -186,7 +186,7 @@ class Assets extends React.Component {
 
         return (
             <div className="grid-block vertical">
-                <div className="grid-block page-layout">
+                <div className="grid-block page-layout vertical">
                     <div className="grid-block main-content small-12 medium-10 medium-offset-1 main-content vertical">
                         <div className="generic-bordered-box">
                             <Tabs
