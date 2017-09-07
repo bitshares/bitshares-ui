2af76b7b7f46ffa2c2bd896dd9567a2efe8b9bb0
Sigve Kvalsvik <svk31@users.noreply.github.com>
2017-09-07 14:48:49 +0200

    Refactor build process using electron-builder

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ module.exports = function(env) {
     if (env.prod) {
         // PROD OUTPUT PATH
         let outputDir = env.electron ? "electron" : env.hash ? "hash-history" : "dist";
-        outputPath = path.join(root_dir, "release", outputDir);
+        outputPath = path.join(root_dir, "build", outputDir);
 
         // DIRECTORY CLEANER
         var cleanDirectories = [outputPath];
