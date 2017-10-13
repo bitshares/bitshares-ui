0718451a11627ee93ddc498cbfa6691c409f82e8
Sigve Kvalsvik <svk31@users.noreply.github.com>
2017-10-13 10:03:21 +0200

    Remove fees_pending and fees_vested text, update fee texts #294

--- app/components/Account/Statistics.jsx
+++ app/components/Account/Statistics.jsx
@@ -18,14 +18,6 @@ class Statistics extends React.Component {
                     <td><Translate content="account.member.fees_paid"/> </td>
                     <td><FormattedAsset amount={parseFloat(stat_object.lifetime_fees_paid)} asset="1.3.0" /></td>
                 </tr>
-                <tr>
-                    <td><Translate content="account.member.fees_pending"/> </td>
-                    <td><FormattedAsset amount={parseFloat(stat_object.pending_fees)} asset="1.3.0" /></td>
-                </tr>
-                <tr>
-                    <td><Translate content="account.member.fees_vested"/> </td>
-                    <td><FormattedAsset amount={parseFloat(stat_object.pending_vested_fees)} asset="1.3.0" /></td>
-                </tr>
             </tbody>
         );
     }
