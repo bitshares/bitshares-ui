a995d5b9e8b40d9182569075c54b74979f64c9d5
Sigve Kvalsvik <svk31@users.noreply.github.com>
2017-11-01 13:33:02 +0100

    Change the faucet to faucet.bitshares.eu, fix some account creation chaining issues #650

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -31,7 +31,7 @@ export const settingsAPIs = {
         {url: "wss://bitshares.crypto.fans/ws", location: "Munich, Germany"},
         {url: "wss://node.testnet.bitshares.eu", location: "Public Testnet Server (Frankfurt, Germany)"}
     ],
-    DEFAULT_FAUCET: "https://bitshares.openledger.info",
+    DEFAULT_FAUCET: "https://faucet.bitshares.eu",
     TESTNET_FAUCET: "https://faucet.testnet.bitshares.eu",
     RPC_URL: "https://openledger.info/api/"
 };
