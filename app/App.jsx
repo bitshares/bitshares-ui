6b05316ea136c96493e50b4ed1b6ea490736b8ea
Salvatore Ingala <salvatore.ingala@gmail.com>
2017-11-07 17:44:29 +0000

    Refactoring of the i18n

--- app/App.jsx
+++ app/App.jsx
@@ -221,7 +221,7 @@ class RootIntl extends React.Component {
     render() {
         return (
             <IntlProvider
-                locale={this.props.locale.replace(/cn/, "zh")}
+                locale={this.props.locale}
                 formats={intlData.formats}
                 initialNow={Date.now()}
             >
