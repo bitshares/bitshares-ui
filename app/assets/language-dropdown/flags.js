e68517ce8370da537c55ebea5d4e64cd15e97f2e
Salvatore Ingala <salvatore.ingala@gmail.com>
2017-11-09 10:56:34 +0000

    Refactoring of the i18n; closes #654 (#709)
    * Refactoring of the i18n
    
    * Renamed 'cn' help folder to 'zh', following up on the locale code change

--- app/assets/language-dropdown/flags.js
+++ app/assets/language-dropdown/flags.js
@@ -2,7 +2,7 @@
 require("file-loader?name=language-dropdown/EN.[ext]!./img/US.png");
 require("file-loader?name=language-dropdown/[name].[ext]!./img/FR.png");
 require("file-loader?name=language-dropdown/[name].[ext]!./img/TR.png");
-require("file-loader?name=language-dropdown/[name].[ext]!./img/CN.png");
+require("file-loader?name=language-dropdown/[name].[ext]!./img/ZH.png");
 require("file-loader?name=language-dropdown/[name].[ext]!./img/DE.png");
 require("file-loader?name=language-dropdown/[name].[ext]!./img/IT.png");
 require("file-loader?name=language-dropdown/KO.[ext]!./img/KR.png");
