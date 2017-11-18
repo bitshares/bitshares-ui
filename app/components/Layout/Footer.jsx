848e19006408fd327fd985102d5ed61ffb5b576b
startailcoon <startail@outlook.com>
2017-11-18 16:50:32 +0100

    Fix

--- app/components/Layout/Footer.jsx
+++ app/components/Layout/Footer.jsx
@@ -79,7 +79,7 @@ class Footer extends React.Component {
 
         // Current Node Details
         let currentNode = SettingsStore.getState().settings.get("activeNode");
-        let currentNodePing = SettingsStore.getState().apiLatencies[currentNode] ? SettingsStore.getState().apiLatencies[currentNode] : "-";
+        let currentNodePing = SettingsStore.getState().apiLatencies[currentNode];
 
         let block_height = this.props.dynGlobalObject.get("head_block_number");
         let version_match = APP_VERSION.match(/2\.0\.(\d\w+)/);
@@ -138,7 +138,7 @@ class Footer extends React.Component {
                             <div className="footer-block">
                                 <span>
                                     <span className="footer-block-title"><Translate content="footer.latency" /></span>
-                                        &nbsp;{!connected ? "-" : currentNodePing + "ms"}&nbsp;/&nbsp;
+                                        &nbsp;{!connected ? "-" : !currentNodePing ? "-" : currentNodePing + "ms"}&nbsp;/&nbsp;
                                     <span className="footer-block-title"><Translate content="footer.block" /></span>
                                         &nbsp;#{block_height}
                                 </span>
