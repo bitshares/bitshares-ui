29348817699c6ccdd1a6b1b177f4947b5384f948
Calvin Froedge <calvinfroedge@gmail.com>
2017-11-22 12:50:29 -0500

    display quoted price in open orders

---
+++ app/components/Utility/MarketPrice.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import ChainTypes from "../Utility/ChainTypes";
+import BindToChainState from "../Utility/BindToChainState";
+import cnames from "classnames";
+import MarketsActions from "actions/MarketsActions";
+import MarketsStore from "stores/MarketsStore";
+import { connect } from "alt-react";
+import utils from "common/utils";
+import FormattedPrice from "./FormattedPrice";
+
+class MarketPrice extends React.Component {
+
+    static propTypes = {
+        quote: ChainTypes.ChainAsset.isRequired,
+        base: ChainTypes.ChainAsset.isRequired,
+        invert: React.PropTypes.bool
+    };
+
+    static defaultProps = {
+        invert: true
+    };
+
+    constructor() {
+        super();
+
+        this.statsInterval = null;
+    }
+
+    _checkStats(newStats = {close: {}}, oldStats = {close: {}}) {
+        return (
+            newStats.volumeBase !== oldStats.volumeBase ||
+            !utils.are_equal_shallow(newStats.close && newStats.close.base, oldStats.close && oldStats.close.base) ||
+            !utils.are_equal_shallow(newStats.close && newStats.close.quote, oldStats.close && oldStats.close.quote)
+        );
+    }
+
+    shouldComponentUpdate(np) {
+        return (
+            this._checkStats(np.marketStats, this.props.marketStats) ||
+            np.base.get("id") !== this.props.base.get("id") ||
+            np.quote.get("id") !== this.props.quote.get("id")
+        );
+    }
+
+    componentWillMount() {
+        MarketsActions.getMarketStats.defer(this.props.quote, this.props.base);
+        this.statsChecked = new Date();
+        this.statsInterval = setInterval(MarketsActions.getMarketStats.bind(this, this.props.quote, this.props.base), 35 * 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.statsInterval);
+    }
+
+    render() {
+        let {marketStats, marketStatsInverted} = this.props;
+        let price = marketStats && marketStats.price ? marketStats.price : null;
+        if (!price && marketStatsInverted && marketStatsInverted.price) {
+            price = marketStatsInverted.price.invert();
+        }
+
+        return (
+            <span className={cnames("", this.props.className)}>
+                {price ?
+                    <FormattedPrice
+                        base_amount={price.base.amount} base_asset={price.base.asset_id}
+                        quote_amount={price.quote.amount} quote_asset={price.quote.asset_id}
+                        hide_symbols
+                    />
+                    : "n/a"
+                }
+            </span>
+        );
+    }
+}
+
+MarketPrice = BindToChainState(MarketPrice);
+
+class MarketCardWrapper extends React.Component {
+    render() {
+        return (
+            <MarketPrice {...this.props} />
+        );
+    }
+}
+
+export default connect(MarketCardWrapper, {
+    listenTo() {
+        return [MarketsStore];
+    },
+    getProps(props) {
+        let invertedId = props.marketId.split("_")[1] + "_" + props.marketId.split("_")[0];
+        return {
+            marketStats: MarketsStore.getState().allMarketStats.get(props.marketId),
+            marketStatsInverted: MarketsStore.getState().allMarketStats.get(invertedId)
+        };
+    }
+});
