b7cc3cec7fbba74a59ac4e676d00c25a4695ad3c
Sigve Kvalsvik <svk31@users.noreply.github.com>
2017-12-05 16:40:11 +0100

    Fix settings active state in right menu

--- app/components/Layout/Header.jsx
+++ app/components/Layout/Header.jsx
@@ -395,7 +395,7 @@ class Header extends React.Component {
                                             <div className="table-cell"><Translate content="modal.withdraw.submit" /></div>
                                         </li>
 
-                                        <li className={cnames({active: active.indexOf("/settings") !== -1}), "divider"} onClick={this._onNavigate.bind(this, "/settings")}>
+                                        <li className={cnames({active: active.indexOf("/settings") !== -1}, "divider")} onClick={this._onNavigate.bind(this, "/settings")}>
                                             <div className="table-cell"><Icon size="2x" name="cogs" /></div>
                                             <div className="table-cell"><Translate content="header.settings" /></div>
                                         </li>
