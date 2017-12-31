5008896320dfb1472ab5392ed99ba4fcac3e2697
etiennemarais2 <etienne.marais@gmail.com>
2017-12-31 11:19:07 +0200

    Update apiConfig.js (#918)
    Add full node, South Africa.
    Forum member: etienne_marais
    Proposed witness node (separate chain from submitted node): etienne-marais

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -35,7 +35,8 @@ export const settingsAPIs = {
         {url: "wss://ws.gdex.top", location: "China"},
         {url: "wss://dex.rnglab.org", location: "Netherlands"},
         {url: "wss://dexnode.net/ws", location: "Dallas, USA"},
-        {url: "wss://la.dexnode.net/ws", location: "Los Angeles, USA"}
+        {url: "wss://la.dexnode.net/ws", location: "Los Angeles, USA"},
+        {url: "wss://btsza.co.za:8091/ws", location: "Cape Town, South Africa"}
     ],
     DEFAULT_FAUCET: "https://faucet.bitshares.eu/onboarding",  // 2017-12-infrastructure worker proposal
     TESTNET_FAUCET: "https://faucet.testnet.bitshares.eu",
