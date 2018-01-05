6efaf2ea346cacb54b2dd6ce64ad0e9ab60a4ca2
dot5enko <dot5enko@yandex.ru>
2018-01-05 12:36:44 +0200

    Tweaked vertical algnment of account selector icons

--- app/components/Account/AccountSelector.jsx
+++ app/components/Account/AccountSelector.jsx
@@ -152,7 +152,7 @@ class AccountSelector extends React.Component {
         }        
         
         let linked_status = !this.props.accountName ? null : (linkedAccounts.has(this.props.accountName)) ?
-            <span className="tooltip" data-place="top" data-tip={counterpart.translate("tooltip.follow_user")} onClick={this.onUnLinkAccount.bind(this)}><Icon className={""+(isGreenAccount? " green":"")} style={{position:"absolute",top:"-0.2em",right:".2em"}} name="user" /></span>
+            <span className="tooltip" data-place="top" data-tip={counterpart.translate("tooltip.follow_user")} onClick={this.onUnLinkAccount.bind(this)}><Icon className={""+(isGreenAccount? " green":"")} style={{position:"absolute",top:"-0.3em",right:".2em"}} name="user" /></span>
             : <span className="tooltip" data-place="top" data-tip={counterpart.translate("tooltip.follow_user_add")} onClick={this.onLinkAccount.bind(this)}><Icon style={{position:"absolute",top:"-0.2em",right:".2em"}} name="plus-circle" /></span>;
         
         return (
