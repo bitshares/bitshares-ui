c21b77a2c90373d413322b3e09b26cafabb6bd4d
Leroy Jethro Gibbs <leroygibbsfromncis@gmail.com>
2018-02-07 14:18:11 +0100

    Add total balance for Hidden assets table #1061

--- app/components/Account/AccountOverview.jsx
+++ app/components/Account/AccountOverview.jsx
@@ -580,6 +580,8 @@ class AccountOverview extends React.Component {
 
         includedBalances.push(<tr key="portfolio" className="total-value"><td style={{textAlign: "left"}}>{totalValueText}</td><td></td><td className="column-hide-small"></td><td></td><td className="column-hide-small" style={{textAlign: "right"}}>{portfolioActiveAssetsBalance}</td><td colSpan="9"></td></tr>);
 
+        hiddenBalances.push(<tr key="portfolio" className="total-value"><td style={{textAlign: "left"}}>{totalValueText}</td><td></td><td className="column-hide-small"></td><td></td><td className="column-hide-small" style={{textAlign: "right"}}>{portfolioHiddenAssetsBalance}</td><td colSpan="9"></td></tr>);
+
         let showAssetPercent = settings.get("showAssetPercent", false);
 
         // Find the current Openledger coins
