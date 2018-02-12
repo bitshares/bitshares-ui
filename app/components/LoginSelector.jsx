aee7dbab6ead7a3c1a8aefe2236907d0b0de9329
Dominic Barnard <dom@ahdigital.co.uk>
2018-02-12 09:19:47 +0000

    Added intro.js plugin to project. Imported plugin to footer.jsx. Added help button to launch hints. Added basic SCSS for the help button. Added first dummy hints on login selector page.

--- app/components/LoginSelector.jsx
+++ app/components/LoginSelector.jsx
@@ -45,6 +45,8 @@ class LoginSelector extends React.Component {
         });
     }
 
+
+
     onSelect(route) {
         this.props.router.push("/create-account/" + route);
     }
@@ -92,7 +94,7 @@ class LoginSelector extends React.Component {
                                
                                 <div className="shrink text-center">
                                     <div className="grp-menu-item overflow-visible account-drop-down">
-                                        <div className="grp-menu-item overflow-visible" style={{margin:"0 auto"}}>
+                                        <div className="grp-menu-item overflow-visible" style={{margin:"0 auto"}} data-intro="Choose from 9 different languages to use in the DEX.">
                                         {flagDropdown}
                                         </div>
                                     </div>
@@ -102,7 +104,7 @@ class LoginSelector extends React.Component {
                         
                         {!!childCount ? null :
                         <div className="grid-block account-login-options">
-                            <Link to="/create-account/password" className="button primary">
+                            <Link to="/create-account/password" className="button primary" data-intro="Access the cloud wallet using an account name and password.">
                                 <Translate content="header.create_account" />
                             </Link>
 
@@ -116,40 +118,11 @@ class LoginSelector extends React.Component {
 
                         {!!childCount ? null :
                         <div className="additional-account-options">
-                            <p>Optionally, <Link to="/wallet/backup/restore">restore your account</Link> or create an account using the <Link to="/create-account/wallet">advanced form</Link>.</p>
+                            <p>Optionally, <Link to="/wallet/backup/restore" data-intro="Restore a wallet using your backup file.">restore your account</Link> or create an account using the <Link to="/create-account/wallet" data-intro="Create a local wallet and download any private keys belonging to it.">advanced form</Link>.</p>
                         </div>}
 
                         {this.props.children}
                     </div>
-                {/* <div className="grid-block no-margin no-padding vertical medium-horizontal no-overflow login-selector">
-
-                    {this.state.incognito ? null : <div className="box small-12 medium-5 large-4" onClick={this.onSelect.bind(this, "wallet")}>
-                        <div className="block-content-header" style={{position: "relative"}}>
-                            <Translate content="wallet.wallet_model" component="h4" />
-                        </div>
-                        <div className="box-content">
-                            <Translate content="wallet.wallet_model_1" component="p" />
-                            <Translate content="wallet.wallet_model_2" component="p" />
-
-                            <Translate unsafe content="wallet.wallet_model_3" component="ul" />
-                        </div>
-                        <div className="button"><Link to="/create-account/wallet"><Translate content="wallet.use_wallet" /></Link></div>
-
-                    </div>}
-
-                    <div className="box small-12 medium-5 large-4 vertical" onClick={this.onSelect.bind(this, "password")}>
-                        <div className="block-content-header" style={{position: "relative"}}>
-                            <Translate content="wallet.password_model" component="h4" />
-                        </div>
-                        <div className="box-content">
-                            <Translate content="wallet.password_model_1" component="p" />
-                            <Translate content="wallet.password_model_2" unsafe component="p" />
-
-                            <Translate unsafe content="wallet.password_model_3" component="ul" />
-                        </div>
-                        <div className="button"><Link to="/create-account/password"><Translate content="wallet.use_password" /></Link></div>
-                    </div>
-                </div> */}
                 </div>
             </div>
         );
@@ -166,3 +139,5 @@ export default connect(LoginSelector, {
         };
     }
 });
+
+// export default launchIntroJS();
