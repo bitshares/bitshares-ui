b4dffb938e926f357a069b57276f78a23febcfe2
Leroy Jethro Gibbs <35899973+gibbsfromncis@users.noreply.github.com>
2018-03-14 09:16:21 +0100

    #1258 Add ability to sort Portfolio by QTY (#1313)
    * #1258 Add an ability sort portfolio by QTY
    
    * #1258 Reset sortOrder when click on new row. Update fields to sort from max to min by default
    
    * #1258 Add cursor pointer for QTY col head

--- app/components/Account/AccountOverview.jsx
+++ app/components/Account/AccountOverview.jsx
@@ -64,6 +64,7 @@ class AccountOverview extends React.Component {
             ]
         };
 
+        this.qtyRefs = {};
         this.priceRefs = {};
         this.valueRefs = {};
         this.changeRefs = {};
@@ -73,6 +74,13 @@ class AccountOverview extends React.Component {
     }
 
     sortFunctions = {
+        qty: function(a, b, force) {
+            if (Number(this.qtyRefs[a.key]) < Number(this.qtyRefs[b.key]))
+                return this.state.sortDirection || force ? -1 : 1;
+
+            if (Number(this.qtyRefs[a.key]) > Number(this.qtyRefs[b.key]))
+                return this.state.sortDirection || force ? 1 : -1;
+        },
         alphabetic: function(a, b, force) {
             if (a.key > b.key)
                 return this.state.sortDirection || force ? 1 : -1;
@@ -105,7 +113,7 @@ class AccountOverview extends React.Component {
                 if (aValue && !bValue) return -1;
                 if (!aValue && !bValue)
                     return this.sortFunctions.alphabetic(a, b, true);
-                return !this.state.sortDirection
+                return this.state.sortDirection
                     ? aValue - bValue
                     : bValue - aValue;
             }
@@ -380,6 +388,13 @@ class AccountOverview extends React.Component {
                 backedCoin.withdrawalAllowed &&
                 (hasBalance && balanceObject.get("balance") != 0);
             const canBuy = !!this.props.bridgeCoins.get(symbol);
+
+            const assetAmount = balanceObject.get("balance");
+
+            this.qtyRefs[asset.get("symbol")] = utils.get_asset_amount(
+                assetAmount,
+                asset
+            );
 
             balances.push(
                 <tr key={asset.get("symbol")} style={{maxWidth: "100rem"}}>
@@ -794,6 +809,7 @@ class AccountOverview extends React.Component {
                 portfolioSort: key
             });
             this.setState({
+                sortDirection: false,
                 sortKey: key
             });
         }
@@ -1092,6 +1108,11 @@ class AccountOverview extends React.Component {
                                                     />
                                                 </th>
                                                 <th
+                                                    onClick={this._toggleSortOrder.bind(
+                                                        this,
+                                                        "qty"
+                                                    )}
+                                                    className="clickable"
                                                     style={{textAlign: "right"}}
                                                 >
                                                     <Translate content="account.qty" />
