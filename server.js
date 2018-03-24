d7236fdcc77f0657f6e54a765ec089eecb97fb44
Leroy Jethro Gibbs <leroygibbsfromncis@gmail.com>
2018-03-24 20:30:45 +0100

    #999 Add https server for local development

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var webpack = require("webpack");
 var express = require("express");
 var devMiddleware = require("webpack-dev-middleware");
 var hotMiddleware = require("webpack-hot-middleware");
+var fs = require("fs");
 
 var ProgressPlugin = require("webpack/lib/ProgressPlugin");
 var config = require("./webpack.config.js")({prod: false});
@@ -10,29 +11,42 @@ var config = require("./webpack.config.js")({prod: false});
 var app = express();
 var compiler = webpack(config);
 
-compiler.apply(new ProgressPlugin(function (percentage, msg) {
-    process.stdout.write((percentage * 100).toFixed(2) + '% ' + msg + '                 \033[0G');
-}));
+var https = require("https");
+var http = require("http");
 
-app.use(devMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    historyApiFallback: true
-}));
+compiler.apply(
+    new ProgressPlugin(function(percentage, msg) {
+        process.stdout.write(
+            (percentage * 100).toFixed(2) +
+                "% " +
+                msg +
+                "                 \033[0G"
+        );
+    })
+);
+
+app.use(
+    devMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        historyApiFallback: true
+    })
+);
 
 app.use(hotMiddleware(compiler));
 
-app.get("*", function (req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "app/assets/index-dev.html"));
 });
 
-app.listen(8080, function (err) {
-    if (err) {
-        return console.error(err);
-    }
+var options = {
+    key: fs.readFileSync("./ssl/server.key"),
+    cert: fs.readFileSync("./ssl/server.crt")
+};
 
-    console.log("Listening at http://localhost:8080/");
-});
+http.createServer(app).listen(8080);
+https.createServer(options, app).listen(8085);
 
+console.log("Listening at http://localhost:8080/ or https://localhost:8085/");
 // new WebpackDevServer(compiler, {
 //     publicPath: config.output.publicPath,
 //     hot: true,
