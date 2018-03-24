75b70341336d2f8adf2c12887bcaa588b8d91a59
Sigve Kvalsvik <svk31@users.noreply.github.com>
2018-03-24 10:51:11 +0100

    #1388: Set set_subscribe_callback boolean to false

--- app/components/Account/AccountPermissionsList.jsx
+++ app/components/Account/AccountPermissionsList.jsx
@@ -289,4 +289,4 @@ class AccountPermissionsList extends React.Component {
     }
 }
 
-export default BindToChainState(AccountPermissionsList, {keep_updating: true});
+export default BindToChainState(AccountPermissionsList, {autosubscribe: false});
