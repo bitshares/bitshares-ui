3437ddb763ca2c47c7cb7ca10a218c33616080c5
Calvin Froedge <calvinfroedge@gmail.com>
2018-03-25 09:58:20 -0500

    Remove references to transwiser

--- app/lib/common/account_utils.js
+++ app/lib/common/account_utils.js
@@ -127,8 +127,6 @@ export default class AccountUtils {
             "blocktrade",
             "locktrades",
             "yun.bts",
-            "transwiser-walle",
-            "transwiser-wallets",
             "ranswiser-wallet",
             "yun.btc",
             "pay.coinbase.com",
