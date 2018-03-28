f3763f1772ab6e193d1cd074048b5c3ffdf0b410
Calvin Froedge <calvinfroedge@gmail.com>
2018-03-28 12:46:30 -0500

    Show incognito warning for wallet users

--- app/components/Layout/Incognito.jsx
+++ app/components/Layout/Incognito.jsx
@@ -3,49 +3,16 @@ import Translate from "react-translate-component";
 
 class Incognito extends React.Component {
     render() {
+        const {props} = this;
         return (
-            <div
-                id="incognito"
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    textAlign: "center",
-                    position: "absolute",
-                    background: "black"
-                }}
-            >
-                <div
-                    style={{
-                        height: "50%",
-                        float: "left",
-                        marginBottom: "-150px",
-                        width: "100%"
-                    }}
-                />
-                <Translate
-                    content="incognito.no_support"
-                    style={{
-                        clear: "both",
-                        display: "block",
-                        fontSize: "3em",
-                        width: "50%",
-                        lineHeight: "1.5em",
-                        margin: "0 auto 1em auto"
-                    }}
-                />
-                <button
-                    onClick={this.props.onClickIgnore}
-                    style={{
-                        display: "block",
-                        margin: "0 auto",
-                        width: "50%",
-                        borderRadius: "10px",
-                        padding: "1em",
-                        color: "black"
-                    }}
-                >
-                    <Translate content="incognito.ignore" />
-                </button>
+            <div id="incognito">
+                <div className="dismiss" onClick={props.onClickIgnore}>
+                    &times;
+                </div>
+                <strong>
+                    <Translate content="incognito.mode" />{" "}
+                </strong>
+                <Translate content="incognito.warning" />
             </div>
         );
     }
--- app/lib/feature_detect/incognito.js
+++ app/lib/feature_detect/incognito.js
@@ -1,54 +1,64 @@
-import browser from './browser'
+import browser from "./browser";
 /*
 * Using feature detectino techniques described here:
 * https://stackoverflow.com/questions/2860879/detecting-if-a-browser-is-using-private-browsing-mode
 */
-export default function(cb){
-    return cb(false);
-    // let ua = navigator.userAgent.toLowerCase();
-    // let name = browser();
-    // if(name === "firefox"){
-    //     var db = indexedDB.open("test");
-    //     db.onerror = function(){
-    //         cb(true);
-    //     };
-    //     db.onsuccess =function(){
-    //         cb(false);
-    //     };
-    // } else if(name === "safari"){
-    //     var storage = window.sessionStorage;
-    //     try {
-    //         storage.setItem("someKeyHere", "test");
-    //         storage.removeItem("someKeyHere");
-    //         cb(false);
-    //     } catch (e) {
-    //         if (e.code === DOMException.QUOTA_EXCEEDED_ERR && storage.length === 0) {
-    //             //Private here
-    //             cb(true);
-    //         } else {
-    //             cb(false);
-    //         }
-    //     }
-    // } else if(name === "chrome" || name === "opera"){
-    //     var fs = window.RequestFileSystem || window.webkitRequestFileSystem;
-    //     if (!fs) {
-    //         cb(false);
-    //         return;
-    //     }
-    //
-    //     fs(window.TEMPORARY, 100, function (fs) {
-    //         // Not incognito mode
-    //         cb(false);
-    //     }, function (err) {
-    //         // Incognito mode
-    //         cb(true);
-    //     });
-    // } else if(name === "ie"){
-    //     if(!window.indexedDB && (window.PointerEvent || window.MSPointerEvent)){
-    //         //Privacy Mode
-    //         cb(true);
-    //     } else {
-    //         cb(false);
-    //     }
-    // }
+export default function(cb) {
+    let ua = navigator.userAgent.toLowerCase();
+    let name = browser();
+    if (name === "firefox") {
+        var db = indexedDB.open("test");
+        db.onerror = function() {
+            cb(true);
+        };
+        db.onsuccess = function() {
+            cb(false);
+        };
+    } else if (name === "safari") {
+        var storage = window.sessionStorage;
+        try {
+            storage.setItem("someKeyHere", "test");
+            storage.removeItem("someKeyHere");
+            cb(false);
+        } catch (e) {
+            if (
+                e.code === DOMException.QUOTA_EXCEEDED_ERR &&
+                storage.length === 0
+            ) {
+                //Private here
+                cb(true);
+            } else {
+                cb(false);
+            }
+        }
+    } else if (name === "chrome" || name === "opera") {
+        var fs = window.RequestFileSystem || window.webkitRequestFileSystem;
+        if (!fs) {
+            cb(false);
+            return;
+        }
+
+        fs(
+            window.TEMPORARY,
+            100,
+            function(fs) {
+                //Not incognito mode
+                cb(false);
+            },
+            function(err) {
+                //Incognito mode
+                cb(true);
+            }
+        );
+    } else if (name === "ie") {
+        if (
+            !window.indexedDB &&
+            (window.PointerEvent || window.MSPointerEvent)
+        ) {
+            //Privacy Mode
+            cb(true);
+        } else {
+            cb(false);
+        }
+    }
 }
