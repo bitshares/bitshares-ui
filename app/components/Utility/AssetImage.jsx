12e34327549364d34a1486f0f432a64c623e9220
crymost99 <crymost99@gmail.com>
2018-04-11 11:43:54 +0200

    [EPIC] upstream global asset image component

---
+++ app/components/Utility/AssetImage.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import CryptoBridgeStore from "stores/CryptoBridgeStore";
+import {connect} from "alt-react";
+
+class AssetImage extends React.Component {
+    static propTypes = {
+        name: React.PropTypes.string,
+        marketId: React.PropTypes.string
+    };
+
+    constructor(props) {
+        super();
+
+        this.state = {
+            src: this._getImgSrcFromProps(props)
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.cryptoBridgeAsset !== this.props.cryptoBridgeAsset) {
+            this.setState({src: this._getImgSrcFromProps(nextProps)});
+        }
+    }
+
+    _onImageError() {
+        this.setState({src: null});
+    }
+
+    _getImgSrcFromProps(props) {
+        let {name, marketId, cryptoBridgeAsset} = props;
+
+        if (!name && marketId) {
+            name = marketId.split("_").shift();
+        }
+
+        let imgSrc = cryptoBridgeAsset ? cryptoBridgeAsset.img : null;
+
+        if (!imgSrc) {
+            let imgName = "";
+
+            if (name === "OPEN.BTC") {
+                imgName = name;
+            } else {
+                const imgSplit = name.split(".");
+                imgName = imgSplit.length === 2 ? imgSplit[1] : imgSplit[0];
+
+                if (imgName === "BTC") {
+                    // TODO remove once added to API
+                    imgSrc = "https://crypto-bridge.org/img/btc.png";
+                }
+
+                if (imgName === "BCO") {
+                    // TODO remove once added to API
+                    imgSrc = "/asset-symbols/bco.png";
+                }
+            }
+
+            if (!imgSrc) {
+                imgSrc = `${__BASE_URL__}asset-symbols/${imgName.toLowerCase()}.png`;
+            }
+        }
+
+        return imgSrc;
+    }
+
+    render() {
+        const {style} = this.props;
+        const {src} = this.state;
+
+        return src ? (
+            <img
+                className="align-center"
+                onError={this._onImageError.bind(this)}
+                style={style || {}}
+                src={src}
+            />
+        ) : (
+            <span />
+        );
+    }
+}
+
+export default connect(AssetImage, {
+    listenTo() {
+        return [CryptoBridgeStore];
+    },
+    getProps(props) {
+        const cryptoBridgeAsset = props.marketId
+            ? CryptoBridgeStore.getState().markets.get(props.marketId)
+            : CryptoBridgeStore.getState().assets.get(props.name);
+
+        return {
+            cryptoBridgeAsset
+        };
+    }
+});
