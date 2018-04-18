b63992bcc725db541e537ee4049a765b0f04b64b
RuneStone <rtk@rtk-cv.dk>
2018-04-18 02:01:29 +0200

    Reorganizing nodes by provider and adding node.blockbasis.com

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -30,16 +30,24 @@ export const settingsAPIs = {
     DEFAULT_WS_NODE: "wss://fake.automatic-selection.com",
     WS_NODE_LIST: [
         {url: "wss://fake.automatic-selection.com", location: {translate: "settings.api_closest"}},
-        {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
-        {url: "wss://bitshares.openledger.info/ws", location: "Nuremberg, Germany"},
-        {url: "wss://eu.openledger.info/ws", location: "Berlin, Germany"},
-        {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
-        {url: "wss://bts.ai.la/ws", location: "Hong Kong"},
+        // blockbasis
+        {url: "wss://node.blockbasis.com", location: "Blockbasis - Blockbasis.com"},
+        // apasia
         {url: "wss://bitshares.apasia.tech/ws", location: "Bangkok, Thailand"},
         {url: "wss://japan.bitshares.apasia.tech/ws", location: "Tokyo, Japan"},
+        // Openledger
+        {url: "wss://bitshares.openledger.info/ws", location: "Nuremberg, Germany"},
+        {url: "wss://eu.openledger.info/ws", location: "Berlin, Germany"},
+        {url: "wss://openledger.hk/ws", location: "Hong Kong"},
+        // BitShares Infrastructure Program
+        {url: "wss://eu.nodes.bitshares.ws", location: "Central Europe - BitShares Infrastructure Program"},
+        {url: "wss://us.nodes.bitshares.ws", location: "U.S. West Coast - BitShares Infrastructure Program"},
+        {url: "wss://sg.nodes.bitshares.ws", location: "Singapore - BitShares Infrastructure Program"},
+        // Miscellaneous
+        {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
+        {url: "wss://bts.ai.la/ws", location: "Hong Kong"},
         {url: "wss://bitshares.dacplay.org/ws", location:  "Hangzhou, China"},
         {url: "wss://bitshares-api.wancloud.io/ws", location:  "China"},
-        {url: "wss://openledger.hk/ws", location: "Hong Kong"},
         {url: "wss://bitshares.crypto.fans/ws", location: "Munich, Germany"},
         {url: "wss://ws.gdex.top", location: "China"},
         {url: "wss://dex.rnglab.org", location: "Netherlands"},
@@ -47,13 +55,12 @@ export const settingsAPIs = {
         {url: "wss://kc-us-dex.xeldal.com/ws", location: "Kansas City, USA"},
         {url: "wss://btsza.co.za:8091/ws", location: "Cape Town, South Africa"},
         {url: "wss://api.bts.blckchnd.com", location: "Falkenstein, Germany"},
-        {url: "wss://eu.nodes.bitshares.ws", location: "Central Europe - BitShares Infrastructure Program"},
-        {url: "wss://us.nodes.bitshares.ws", location: "U.S. West Coast - BitShares Infrastructure Program"},
-        {url: "wss://sg.nodes.bitshares.ws", location: "Singapore - BitShares Infrastructure Program"},
         {url: "wss://ws.winex.pro", location: "Singapore"},
         // Testnet
         {url: "wss://node.testnet.bitshares.eu", location: "TESTNET - BitShares Europe (Frankfurt, Germany)"},
         {url: "wss://testnet.nodes.bitshares.ws", location: "TESTNET - BitShares Infrastructure Program"}
+        // Localhost
+        {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
     ],
     DEFAULT_FAUCET: "https://faucet.bitshares.eu/onboarding",  // 2017-12-infrastructure worker proposal
     TESTNET_FAUCET: "https://faucet.testnet.bitshares.eu",
