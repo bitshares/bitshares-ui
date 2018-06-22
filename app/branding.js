96a6a3525548bf269a66554027b34a33540d076c
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2018-06-22 14:49:22 +0200

    docs for branding
    work for Branding template #1613
    
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- app/branding.js
+++ app/branding.js
@@ -1,33 +1,75 @@
+/** This file centralized customization and branding efforts throughout the whole wallet and is meant to facilitate
+ *  the process.
+ *
+ *  @author Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
+ */
+
+/**
+ * Wallet name that is used throughout the UI and also in translations
+ * @returns {string}
+ */
 export function getWalletName() {
     return "BitShares";
 }
 
+/**
+ * URL of this wallet
+ * @returns {string}
+ */
 export function getWalletURL() {
     return "https://wallet.bitshares.org";
 }
 
+/**
+ * Logo that is used throughout the UI
+ * @returns {*}
+ */
 export function getLogo() {
     return require("assets/logo-ico-blue.png");
 }
 
+/**
+ * Default set theme for the UI
+ * @returns {string}
+ */
 export function getDefaultTheme() {
     // possible ["darkTheme", "lightTheme", "midnightTheme"]
     return "darkTheme";
 }
 
+/**
+ * Default login method. Either "password" (for cloud login mode) or "wallet" (for local wallet mode)
+ * @returns {string}
+ */
 export function getDefaultLogin() {
     // possible: one of "password", "wallet"
     return "password";
 }
 
+/**
+ * Default units used by the UI
+ *
+ * @returns {[string,string,string,string,string,string]}
+ */
 export function getUnits() {
     return ["BTS", "USD", "CNY", "BTC", "EUR", "GBP"];
 }
+
+/**
+ * These are the highlighted bases in "My Markets" of the exchange
+ *
+ * @returns {[string]}
+ */
 
 export function getMyMarketsBases() {
     return ["USD", "OPEN.BTC", "CNY", "BTS", "BTC"];
 }
 
+/**
+ * These are the default quotes that are shown after selecting a base
+ *
+ * @returns {[string]}
+ */
 export function getMyMarketsQuotes() {
     return [
         "BTS",
@@ -101,12 +143,31 @@ export function getMyMarketsQuotes() {
     ];
 }
 
+/**
+ * Recognized namespaces of assets
+ *
+ * @returns {[string,string,string,string,string,string,string]}
+ */
 export function getAssetNamespaces() {
     return ["TRADE.", "OPEN.", "METAEX.", "BRIDGE.", "RUDEX.", "GDEX.", "WIN."];
 }
 
+/**
+ * These namespaces will be hidden to the user, this may include "bit" for BitAssets
+ * @returns {[string,string]}
+ */
 export function getAssetHideNamespaces() {
-    return ["OPEN.", "bit"];
+    // e..g "OPEN.", "bit"
+    return [];
+}
+
+/**
+ * Allowed gateways that the user will be able to choose from in Deposit Withdraw modal
+ * @param gateway
+ * @returns {boolean}
+ */
+export function allowedGateway(gateway) {
+    return gateway in ["OPEN", "RUDEX", "WIN", "BRIDGE", "GDEX"];
 }
 
 export function getSupportedLanguages() {
@@ -118,8 +179,4 @@ export function allowedLogin(type) {
 
     // possible: list containing any combination of ["password", "wallet"]
     return type == "password";
-}
-
-export function allowedGateway(gateway) {
-    return gateway in ["OPEN", "RUDEX", "WIN", "BRIDGE", "GDEX"];
 }
