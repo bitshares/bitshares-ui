d1cf8cb5bf146029610ad5e3d51913ab2415c448
TatchCapital <34829200+EdwardTatch@users.noreply.github.com>
2018-10-05 01:43:19 +0200

    Update SettingsStore.js

--- app/stores/SettingsStore.js
+++ app/stores/SettingsStore.js
@@ -426,6 +426,7 @@ class SettingsStore {
                 markets_39f5e2ed: [
                     // TESTNET
                     "PEG.FAKEUSD",
+                    "TATCHCOIN",
                     "BTWTY"
                 ]
             };
