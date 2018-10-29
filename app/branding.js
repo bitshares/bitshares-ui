86f226369e7cd5a9868b7eb9eae5dbf920e14af3
TatchCapital <34829200+EdwardTatch@users.noreply.github.com>
2018-10-29 16:40:26 +0100

    Update branding.js

--- app/branding.js
+++ app/branding.js
@@ -77,7 +77,7 @@ export function getUnits(chainId = "4018d784") {
  */
 
 export function getMyMarketsBases() {
-    return ["BRIDGE.BTC", "BRIDGE.NLG"];
+    return ["BRIDGE.BTC", "BRIDGE.NLG", "OPEN.ETH", "bitEUR", "bitUSD"];
 }
 
 /**
