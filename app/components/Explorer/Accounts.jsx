650e1db484a9771af0764184c3ab3683cc161f7e
Alexandr <alexandrmaslov@16.0.168.192.in-addr.arpa>
2018-11-08 08:55:30 +0200

    Closes issue #2093 - Explore Accounts - Styleguide + Sorting Update

--- app/components/Explorer/Accounts.jsx
+++ app/components/Explorer/Accounts.jsx
@@ -5,105 +5,12 @@ import Immutable from "immutable";
 import Translate from "react-translate-component";
 import AccountActions from "actions/AccountActions";
 import {debounce} from "lodash-es";
-import ChainTypes from "../Utility/ChainTypes";
 import Icon from "../Icon/Icon";
-import BindToChainState from "../Utility/BindToChainState";
 import BalanceComponent from "../Utility/BalanceComponent";
 import AccountStore from "stores/AccountStore";
-import {connect} from "alt-react";
 import LoadingIndicator from "../LoadingIndicator";
-import PaginatedList from "../Utility/PaginatedList";
-
-class AccountRow extends React.Component {
-    static propTypes = {
-        account: ChainTypes.ChainAccount.isRequired
-    };
-
-    static defaultProps = {
-        tempComponent: "tr",
-        autosubscribe: false
-    };
-
-    shouldComponentUpdate(nextProps) {
-        return (
-            nextProps.contacts !== this.props.contacts ||
-            nextProps.account !== this.props.account
-        );
-    }
-
-    _onAddContact(account, e) {
-        e.preventDefault();
-        AccountActions.addAccountContact(account);
-    }
-
-    _onRemoveContact(account, e) {
-        e.preventDefault();
-        AccountActions.removeAccountContact(account);
-    }
-
-    render() {
-        let {account, contacts} = this.props;
-
-        if (!account) return null;
-        let balance = account.getIn(["balances", "1.3.0"]) || null;
-        let accountName = account.get("name");
-
-        return (
-            <tr key={account.get("id")}>
-                <td>{account.get("id")}</td>
-                {contacts.has(accountName) ? (
-                    <td onClick={this._onRemoveContact.bind(this, accountName)}>
-                        <Icon
-                            name="minus-circle"
-                            title="icons.minus_circle.remove_contact"
-                        />
-                    </td>
-                ) : (
-                    <td onClick={this._onAddContact.bind(this, accountName)}>
-                        <Icon
-                            name="plus-circle"
-                            title="icons.plus_circle.add_contact"
-                        />
-                    </td>
-                )}
-                <td>
-                    <Link to={`/account/${accountName}/overview`}>
-                        {accountName}
-                    </Link>
-                </td>
-                <td>
-                    {!balance ? "n/a" : <BalanceComponent balance={balance} />}
-                </td>
-                <td>
-                    {!balance ? (
-                        "n/a"
-                    ) : (
-                        <BalanceComponent
-                            balance={balance}
-                            asPercentage={true}
-                        />
-                    )}
-                </td>
-            </tr>
-        );
-    }
-}
-AccountRow = BindToChainState(AccountRow);
-
-let AccountRowWrapper = props => {
-    return <AccountRow {...props} />;
-};
-
-AccountRowWrapper = connect(AccountRowWrapper, {
-    listenTo() {
-        return [AccountStore];
-    },
-    getProps() {
-        return {
-            contacts: AccountStore.getState().accountContacts
-        };
-    }
-});
+import {Table} from "bitshares-ui-style-guide";
+import {ChainStore} from "bitsharesjs";
 
 class Accounts extends React.Component {
     constructor(props) {
@@ -140,13 +47,152 @@ class Accounts extends React.Component {
         this.setState({isLoading: false});
     }
 
+    _onAddContact(account, e) {
+        e.preventDefault();
+        AccountActions.addAccountContact(account);
+        this.forceUpdate();
+    }
+
+    _onRemoveContact(account, e) {
+        e.preventDefault();
+        AccountActions.removeAccountContact(account);
+        this.forceUpdate();
+    }
+
     render() {
         let {searchAccounts} = this.props;
         let {searchTerm} = this.state;
-        let accountRows = [];
+
+        let dataSource = [];
+        let columns = [];
+
+        columns = [
+            {
+                title: (
+                    <Translate component="span" content="explorer.assets.id" />
+                ),
+                dataIndex: "accountId",
+                key: "accountId",
+                defaultSortOrder: "ascend",
+                sorter: (a, b) => {
+                    return a.accountId > b.accountId
+                        ? 1
+                        : a.accountId < b.accountId
+                            ? -1
+                            : 0;
+                },
+                render: id => {
+                    return <div>{id}</div>;
+                }
+            },
+            {
+                title: <Icon name="user" title="icons.user.account" />,
+                dataIndex: "accountContacts",
+                key: "accountContacts",
+                render: (contacts, record) => {
+                    return contacts.has(record.accountName) ? (
+                        <div
+                            onClick={this._onRemoveContact.bind(
+                                this,
+                                record.accountName
+                            )}
+                        >
+                            <Icon
+                                name="minus-circle"
+                                title="icons.minus_circle.remove_contact"
+                            />
+                        </div>
+                    ) : (
+                        <div
+                            onClick={this._onAddContact.bind(
+                                this,
+                                record.accountName
+                            )}
+                        >
+                            <Icon
+                                name="plus-circle"
+                                title="icons.plus_circle.add_contact"
+                            />
+                        </div>
+                    );
+                }
+            },
+            {
+                title: <Translate component="span" content="account.name" />,
+                dataIndex: "accountName",
+                key: "accountName",
+                sorter: (a, b) => {
+                    return a.accountName > b.accountName
+                        ? 1
+                        : a.accountName < b.accountName
+                            ? -1
+                            : 0;
+                },
+                render: name => {
+                    return (
+                        <div>
+                            <Link to={`/account/${name}/overview`}>{name}</Link>
+                        </div>
+                    );
+                }
+            },
+            {
+                title: <Translate component="span" content="gateway.balance" />,
+                dataIndex: "accountBalance",
+                key: "accountBalance",
+                sorter: (a, b) => {
+                    a.accountBalance = parseFloat(a.accountBalance);
+                    b.accountBalance = parseFloat(b.accountBalance);
+                    return a.accountBalance > b.accountBalance
+                        ? 1
+                        : a.accountBalance < b.accountBalance
+                            ? -1
+                            : 0;
+                },
+                render: balance => {
+                    return (
+                        <div>
+                            {!balance ? (
+                                "n/a"
+                            ) : (
+                                <BalanceComponent balance={balance} />
+                            )}
+                        </div>
+                    );
+                }
+            },
+            {
+                title: <Translate component="span" content="account.percent" />,
+                dataIndex: "accountPercentages",
+                key: "accountPercentages",
+                sorter: (a, b) => {
+                    a.accountPercentages = parseFloat(a.accountPercentages);
+                    b.accountPercentages = parseFloat(b.accountPercentages);
+                    return a.accountPercentages > b.accountPercentages
+                        ? 1
+                        : a.accountPercentages < b.accountPercentages
+                            ? -1
+                            : 0;
+                },
+                render: balance => {
+                    return (
+                        <div>
+                            {!balance ? (
+                                "n/a"
+                            ) : (
+                                <BalanceComponent
+                                    balance={balance}
+                                    asPercentage={true}
+                                />
+                            )}
+                        </div>
+                    );
+                }
+            }
+        ];
 
         if (searchAccounts.size > 0 && searchTerm && searchTerm.length > 0) {
-            accountRows = searchAccounts
+            searchAccounts
                 .filter(a => {
                     /*
                     * This appears to return false negatives, perhaps from
@@ -168,10 +214,23 @@ class Accounts extends React.Component {
                         return 0;
                     }
                 })
-                .map((account, id) => {
-                    return <AccountRowWrapper key={id} account={account} />;
-                })
-                .toArray();
+                .map((name, id) => {
+                    let currentAccount = ChainStore.getAccount(
+                        id.toLowerCase()
+                    );
+                    let balance = currentAccount
+                        ? currentAccount.getIn(["balances", "1.3.0"]) || null
+                        : null;
+
+                    dataSource.push({
+                        accountId: id,
+                        accountContacts: AccountStore.getState()
+                            .accountContacts,
+                        accountName: name,
+                        accountBalance: balance,
+                        accountPercentages: balance
+                    });
+                });
         }
 
         return (
@@ -188,43 +247,15 @@ class Accounts extends React.Component {
                             onChange={this._onSearchChange.bind(this)}
                         />
                     </div>
-                    <PaginatedList
-                        header={
-                            <tr>
-                                <th>
-                                    <Translate
-                                        component="span"
-                                        content="explorer.assets.id"
-                                    />
-                                </th>
-                                <th>
-                                    <Icon
-                                        name="user"
-                                        title="icons.user.account"
-                                    />
-                                </th>
-                                <th>
-                                    <Translate
-                                        component="span"
-                                        content="account.name"
-                                    />
-                                </th>
-                                <th>
-                                    <Translate
-                                        component="span"
-                                        content="gateway.balance"
-                                    />
-                                </th>
-                                <th>
-                                    <Translate
-                                        component="span"
-                                        content="account.percent"
-                                    />
-                                </th>
-                            </tr>
-                        }
-                        rows={accountRows}
-                        pageSize={20}
+                    <Table
+                        style={{width: "100%", marginTop: "16px"}}
+                        rowKey="accountId"
+                        columns={columns}
+                        dataSource={dataSource}
+                        pagination={{
+                            position: "bottom",
+                            pageSize: Number(this.state.rowsOnPage)
+                        }}
                     />
                     {this.state.isLoading ? (
                         <div style={{textAlign: "center", padding: 10}}>
