b2cd7bca8ec79bd5509e47b6738f32320ee12ed3
TatchCapital <34829200+EdwardTatch@users.noreply.github.com>
2018-12-18 21:33:55 +0100

    Update branding.js

--- app/branding.js
+++ app/branding.js
@@ -79,7 +79,7 @@ export function getUnits(chainId = "4018d784") {
  */
 
 export function getMyMarketsBases() {
-    return ["TATCH.USD", "TATCH.EUR", "TATCH.NLG", "TATCH.BTC", "BTS"];
+    return ["TATCH.USD", "TATCH.EUR", "TATCH.NLG", "TATCH.BTC", "BTS", "TATCHCOIN"];
 }
 
 /**
