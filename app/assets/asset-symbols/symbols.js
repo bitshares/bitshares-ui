65c1585d08c53c2bbb18a0e1b9331f4fcfe87e7f
TatchCapital <34829200+EdwardTatch@users.noreply.github.com>
2018-12-18 19:52:46 +0100

    Update symbols.js

--- app/assets/asset-symbols/symbols.js
+++ app/assets/asset-symbols/symbols.js
@@ -7,7 +7,7 @@ require("file-loader?name=asset-symbols/[name].png!./eur.png");
 require("file-loader?name=asset-symbols/[name].png!./cny.png");
 require("file-loader?name=asset-symbols/[name].png!./gold.png");
 require("file-loader?name=asset-symbols/[name].png!./btc.png");
-require("file-loader?name=asset-symbols/silver.png!./silver.png");
+require("file-loader?name=asset-symbols/silver.png!./nlg.png");
 
 // 3rd party assets
 require("file-loader?name=asset-symbols/[name].png!./eth.png");
