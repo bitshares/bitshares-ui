f549630413d02e766b73b2ed486e185dee3b4e5c
TatchCapital <34829200+EdwardTatch@users.noreply.github.com>
2018-12-18 22:13:53 +0100

    Update branding.js

--- app/branding.js
+++ app/branding.js
@@ -189,7 +189,7 @@ export function getAssetHideNamespaces() {
  */
 export function allowedGateway(gateway) {
     return (
-        ["TATCH", "BRIDGE", "OPEN"]
+        ["TATCH", "BRIDGE", "OPEN", "tatch"]
 
     );
 }
