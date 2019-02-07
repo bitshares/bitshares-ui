70499ed39457b0827b5850e33377dbcab4a86324
Stefan Schießl <stefan.schiessl@blockchainprojectsbv.com>
2019-02-07 21:45:21 +0100

    clear user input
    Signed-off-by: Stefan Schießl <stefan.schiessl@blockchainprojectsbv.com>

--- app/components/Explorer/CommitteeMembers.jsx
+++ app/components/Explorer/CommitteeMembers.jsx
@@ -9,6 +9,7 @@ import SettingsActions from "actions/SettingsActions";
 import FormattedAsset from "../Utility/FormattedAsset";
 import SettingsStore from "stores/SettingsStore";
 import {Icon, Input, Table} from "bitshares-ui-style-guide";
+import sanitize from "sanitize";
 
 class CommitteeMemberList extends React.Component {
     static propTypes = {
@@ -77,7 +78,10 @@ class CommitteeMemberList extends React.Component {
                         rank: ranks[a.get("id")],
                         name: account.get("name"),
                         votes: account_data.get("total_votes"),
-                        url: account_data.get("url")
+                        url: sanitize(account_data.get("url"), {
+                            whiteList: [], // empty, means filter out all tags
+                            stripIgnoreTag: true // filter out all HTML not in the whilelist
+                        })
                     };
                 });
         }
@@ -227,20 +231,23 @@ class CommitteeMembersStoreWrapper extends React.Component {
     }
 }
 
-CommitteeMembersStoreWrapper = connect(CommitteeMembersStoreWrapper, {
-    listenTo() {
-        return [SettingsStore];
-    },
-    getProps() {
-        return {
-            cardView: SettingsStore.getState().viewSettings.get(
-                "cardViewCommittee"
-            ),
-            filterCommitteeMember: SettingsStore.getState().viewSettings.get(
-                "filterCommitteeMember"
-            )
-        };
+CommitteeMembersStoreWrapper = connect(
+    CommitteeMembersStoreWrapper,
+    {
+        listenTo() {
+            return [SettingsStore];
+        },
+        getProps() {
+            return {
+                cardView: SettingsStore.getState().viewSettings.get(
+                    "cardViewCommittee"
+                ),
+                filterCommitteeMember: SettingsStore.getState().viewSettings.get(
+                    "filterCommitteeMember"
+                )
+            };
+        }
     }
-});
+);
 
 export default CommitteeMembersStoreWrapper;
