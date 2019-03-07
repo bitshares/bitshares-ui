3b8c7cf1b650656ccbb3fc92518130935f85d1ad
VerevkinAlexander <verevkin.alex@apriorit.com>
2019-03-07 18:11:25 +0200

    started table

--- app/components/Showcases/DirectDebit.jsx
+++ app/components/Showcases/DirectDebit.jsx
@@ -8,7 +8,8 @@ import {
     Button,
     Switch,
     Tooltip,
-    Icon
+    Icon,
+    Table
 } from "bitshares-ui-style-guide";
 import AccountSelector from "../Account/AccountSelector";
 import counterpart from "counterpart";
@@ -78,22 +79,86 @@ export default class DirectDebit extends Component {
         });
     };
 
+    _onFilter = e => {
+        // TODO:
+        e.preventDefault();
+    };
+
     render() {
         const {isModalVisible} = this.state;
         console.log("isModalVisible", isModalVisible);
 
         let smallScreen = window.innerWidth < 850 ? true : false;
 
+        const dataSource = [
+            {
+                key: "1",
+                name: "Mike",
+                age: 32,
+                address: "10 Downing Street"
+            },
+            {
+                key: "2",
+                name: "John",
+                age: 42,
+                address: "10 Downing Street"
+            }
+        ];
+
+        const columns = [
+            {
+                title: "Name",
+                dataIndex: "name",
+                key: "name"
+            },
+            {
+                title: "Age",
+                dataIndex: "age",
+                key: "age"
+            },
+            {
+                title: "Address",
+                dataIndex: "address",
+                key: "address"
+            }
+        ];
+
         return (
             <Card>
                 <Row>
-                    <Col span={12} style={{padding: "10px"}}>
-                        <Button onClick={this.showModal}>+</Button>
-                    </Col>
-                    <Col span={12} style={{padding: "10px"}}>
-                        Hi
+                    <Col span={24} style={{padding: "10px"}}>
+                        {/* TABLE HEADER */}
+                        <div
+                            style={{
+                                marginBottom: "30px"
+                            }}
+                        >
+                            <Input
+                                placeholder={counterpart.translate(
+                                    "explorer.witnesses.filter_by_name"
+                                )}
+                                onChange={this._onFilter}
+                                style={{
+                                    width: "200px",
+                                    marginRight: "30px"
+                                }}
+                                addonAfter={<Icon type="search" />}
+                            />
+                            <Button onClick={this.showModal}>
+                                {counterpart.translate(
+                                    "showcases.direct_debit.create_new_mandate"
+                                )}
+                            </Button>
+                        </div>
+
+                        <Table
+                            columns={columns}
+                            dataSource={dataSource}
+                            pagination={false}
+                        />
                     </Col>
                 </Row>
+
                 <DirectDebitModal
                     isModalVisible={isModalVisible}
                     hideModal={this.hideModal}
