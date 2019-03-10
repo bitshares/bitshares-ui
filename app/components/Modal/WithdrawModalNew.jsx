b1154d8c37e44b43df9180ad08ba9b1db50513e7
Startail the 'Coon <startail@outlook.com>
2019-03-10 20:24:33 +0100

    Deposit Modal Enhancements - Gateways drop-down sorted alphabetically - Gateways drop-down lists asset pair balance

--- app/components/Modal/WithdrawModalNew.jsx
+++ app/components/Modal/WithdrawModalNew.jsx
@@ -1028,17 +1028,19 @@ class WithdrawModalNew extends React.Component {
                         <div style={{marginBottom: "1em"}}>
                             {selectedGateway
                                 ? gatewaySelector.call(this, {
-                                      selectedGateway,
-                                      gatewayStatus,
-                                      nAvailableGateways,
-                                      availableGateways:
-                                          coinToGatewayMapping[selectedAsset],
-                                      error: false,
-                                      onGatewayChanged: this.onGatewayChanged.bind(
-                                          this
-                                      )
-                                  })
-                                : null}
+                                    selectedGateway,
+                                    gatewayStatus,
+                                    nAvailableGateways,
+                                    availableGateways:
+                                        coinToGatewayMapping[selectedAsset],
+                                    error: false,
+                                    onGatewayChanged: this.onGatewayChanged.bind(
+                                        this
+                                    ),
+                                    selectedAsset,
+                                    balances,
+                                    assets
+                                }) : null}
                         </div>
 
                         {/*QUANTITY*/}
