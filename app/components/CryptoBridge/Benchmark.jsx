f86a80849453b3908d18b5c5dd160bc487d1a5ee
crymost99 <crymost99@gmail.com>
2019-03-26 09:47:11 +0100

    [FIX] CB-1617 benchmark layout & wording

--- app/components/CryptoBridge/Benchmark.jsx
+++ app/components/CryptoBridge/Benchmark.jsx
@@ -13,54 +13,55 @@ class Benchmark extends React.Component {
                         />
 
                         <p>
-                            CryptoBridge and DARC are pleased to announce
-                            another step forward in their respective
-                            development. Through a joint effort, we will begin
-                            to bring greater attention to the need for higher
-                            quality projects in the crowded digital asset space.
-                            Toward this end, DARC has recently begun offering
-                            project evaluations and CryptoBridge seeks to use
-                            its visibility and position gained in the previous
-                            year to support that service and forward-leaning
-                            perspective.
+                            Benchmarking is available to projects which meet
+                            common quality criteria determined by CryptoBridge
+                            and DARC. Through this joint effort, we bring
+                            greater attention to quality projects to provide
+                            additional recognition in the crowded digital asset
+                            space. Toward this end, DARC offers in-depth project
+                            evaluations while CryptoBridge seeks to use its
+                            visibility and position to support that service to
+                            reinforce a shared, forward-leaning perspective.
                         </p>
 
                         <p>
-                            Our purpose is to recognize projects which meet or
-                            exceed a baseline of fundamental quality. Through
-                            our shared belief in the importance of rigorous and
-                            objective assessment, we will work to foster an
-                            environment which supports the digital asset
-                            landscape’s maturity. Such an endeavor will benefit
-                            project communities, currency and token users,
-                            investors and associated enterprises.
+                            Our collaborative purpose with benchmarking is to
+                            recognize projects which meet or exceed a baseline
+                            of fundamental quality. Citing the importance of
+                            rigorous and objective assessment, we will work to
+                            foster an environment which supports maturity from
+                            within the digital asset landscape. Such an endeavor
+                            will benefit project communities, currency and token
+                            users, investors and associated enterprises.
                         </p>
 
                         <p>
-                            A fee-based but unbiased evaluation will initially
-                            be conducted by DARC to determine projects which
+                            A fee-based but unbiased evaluation is available and
+                            conducted by DARC to determine projects which
                             sufficiently demonstrate that they are worthy of
-                            “benchmark” status and supplemental information
-                            related will then become available on the
-                            evaluator’s website. A smaller subset of common
-                            criteria will be confirmed by CryptoBridge shortly
-                            after publication of the results to validate the
-                            results. Once the agreement is finalized, the
-                            project’s status will recognized by each member of
-                            the partnership on their websites and associated
-                            social media outlets.
+                            benchmark status. Following its completion, a
+                            supplemental report and other information then
+                            becomes available on the evaluator’s website. A
+                            smaller subset of common criteria will be confirmed
+                            by CryptoBridge shortly after publication to
+                            validate the results. Once the agreement is
+                            finalized, the project’s status will recognized by
+                            each member of the partnership on their websites and
+                            associated social media outlets.
                         </p>
 
                         <p>
                             Upon benchmarking, projects will enjoy a range of
-                            benefits: higher visibility on the exchange, a
-                            summary of their achievement (publicized on social
-                            media and blogs), increased consumer confidence,
-                            etc. These projects will be required to maintain or
-                            improve upon these criteria in follow-up evaluations
-                            conducted periodically. This process is designed to
-                            ensure objectivity and transparency and all results
-                            will be communicated as such.
+                            benefits: higher visibility on the trading
+                            interface, a summary of their achievement
+                            (publicized on social media and blogs) and increased
+                            consumer and investor confidence. These projects
+                            will be required to maintain or improve upon these
+                            criteria in follow-up evaluations conducted
+                            periodically to maintain benchmark status. This
+                            process is designed to ensure objectivity and
+                            transparency and all results will be communicated as
+                            such.
                         </p>
 
                         <p>
@@ -76,6 +77,24 @@ class Benchmark extends React.Component {
                             see what positive developments this endeavor will
                             kick-start, and we hope you’ll share our enthusiasm
                             in doing so.
+                        </p>
+
+                        <p>
+                            To view the full range of DARC evaluations{" "}
+                            <a
+                                href="https://www.darc.network/research"
+                                target={"_blank"}
+                            >
+                                click here
+                            </a>.
+                        </p>
+
+                        <p>
+                            For more information about having your project
+                            reviewed, contact{" "}
+                            <a href="mailto:benchmark@crypto-bridge.org">
+                                benchmark@crypto-bridge.org
+                            </a>.
                         </p>
                     </div>
                 </div>
