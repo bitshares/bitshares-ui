38c6b00fa1fdbd1a3b76fc352c9bd0df38ba47f4
Mateusz Otrebski <motrebski@syncad.com>
2019-04-02 20:19:34 +0200

    Update BEOS text

--- app/components/DepositWithdraw/BitsharesBeos.jsx
+++ app/components/DepositWithdraw/BitsharesBeos.jsx
@@ -72,71 +72,90 @@ class BitsharesBeos extends React.Component {
         return (
             <div>
                 <p>
-                    To participate in the BEOS reward program, you must deposit
-                    BTS via this gateway to your BEOS network account. If you
-                    don’t already have a BEOS account, you can create one via
-                    this gateway by paying a fee of {beosFee} BTS. The gateway
-                    will read the public keys of the BitShares account you send
-                    the BTS from, and automatically assign the same keys to your
-                    newly created BEOS account. This means you can use the same
-                    private keys to control both your BitShares account and your
-                    BEOS account. If you don’t want to use the same keys, you
-                    can first create a new BitShares account and send the BTS
-                    from that account to start with a new set of keys. You can
-                    also change your keys at any time on either blockchain.
+                    To qualify for the initial BEOS token distribution, a member
+                    must join BEOS LCA (BLCA is Utah Limited Cooperative
+                    Association, operating on a non-profit basis for the benefit
+                    of its members), create an account on the BEOS network (the
+                    “BEOS Account”) and fund that BEOS account with any amount
+                    of BTS via the BEOS network gateway.
                 </p>
                 <p>
-                    When you deposit BTS via the gateway, your BEOS account will
-                    be credited with an equivalent amount of a BTS IOU token
-                    (minus {beosFee} BTS if you are simultaneously creating your
-                    BEOS account).
+                    Any member may create one or more BEOS accounts for a
+                    one-time fee of {beosFee} BTS each. The BEOS network gateway
+                    will read the public keys of the Bitshares account from
+                    which the account creation fee is paid and automatically
+                    assign the same keys to the Member’s newly created BEOS
+                    account, so that the member can use the same private keys to
+                    control both accounts. Thereafter the member may change
+                    their keys at any time on either account.
                 </p>
                 <p>
-                    During the 89 days of the BEOS reward distribution period,
-                    starting on March 25th 2019 (one week after the launch date
-                    for the BEOS network), a fixed amount of BEOS will be
-                    awarded hourly to BEOS accounts holding BTS IOU tokens,
-                    based on their proportionate holding. For example, if there
-                    are only 2 BEOS accounts at the end of an hourly reward
-                    period, one holding 90 tokens and the other holding 10
-                    tokens, the 90 holder will receive 90% of the BEOS awarded
-                    for that reward period and the 10 holder will receive the
-                    remaining 10%. 3674470000 BEOS will be distributed during
-                    this period, with 2/7 being placed in the BEOS trustee
-                    account to fund future development and support of the chain,
-                    and the remaining 5/7 to be distributed to BTS depositors.
+                    When a member deposits BTS via the BEOS network gateway, the
+                    member’s BEOS account is credited with an equivalent amount
+                    of a BTS IOU token. BLCA will retain 2/7 of the
+                    3,674,470,000 authorized BEOS tokens in order to promote
+                    Association purposes. The remainder will be distributed to
+                    Members.
                 </p>
                 <p>
-                    In a similar manner, RAM will also be awarded to BTS IOU
-                    holders, but over a longer period of time: 64GB of RAM will
-                    be distributed over a 2 year period.
+                    BEOS tokens will be distributed to account holders on an
+                    hourly basis over a period of 89 days and 64GB of RAM will
+                    be distributed over an 80-week. Both periods begin on April
+                    9, 2019.
                 </p>
                 <p>
-                    IMPORTANT NOTE: Until you make a withdrawal, the “actual
-                    BTS” deposited to the gateway will be held in a gateway
-                    account called “beos” on the BitShares network. This
-                    multi-sig account is managed by the BEOS trustees, and by
-                    making deposits to this gateway, you are explicitly granting
-                    the trustees the right to vote the BTS you deposit in ways
-                    that support the health and growth of the BEOS network. You
-                    have the option of withdrawing your BTS IOU tokens via the
-                    gateway at any time (*) back to the BTS network, if you
-                    disagree with the way they are voting or if you just need
-                    your BTS back for some other purpose.
+                    During the applicable distribution periods, BEOS and RAM
+                    tokens will be apportioned between member account holders
+                    based upon their proportionate holdings of BTS IOU tokens.
+                    For example, if there are only 2 BEOS accounts at the end of
+                    an hourly reward period, one holding 90 tokens and the other
+                    holding 10 tokens, the 90 holder will receive 90% of the
+                    BEOS awarded for that reward period and the 10 holder will
+                    receive the remaining 10%.
                 </p>
                 <p>
-                    * The trustees will do their best to keep the gateway
+                    BEOS tokens are backed by BLCA Quint holdings, such that 1
+                    million BEOS tokens equals 1 Quint. For more information,
+                    see{" "}
+                    <a href="http://www.quintric.com" target="_blank">
+                        www.quintric.com
+                    </a>
+                    .
+                </p>
+                <p>
+                    IMPORTANT NOTE : Until a withdrawal is made, the “actual
+                    BTS” deposited to the BEOS network gateway will be held in
+                    an account called “beos.gateway” on the Bitshares network.
+                    This account is managed by the Board, and by making deposits
+                    to this account, a Member explicitly grants the Board the
+                    right to vote the BTS so deposited in ways that support the
+                    health and growth of the BEOS network.
+                </p>
+                <p>
+                    Members have the option of withdrawing their BTS IOU tokens
+                    via the BEOS network gateway at any time (*) back to their
+                    own control.
+                </p>
+                <p>
+                    * The BLCA trustees will do their best to keep the gateway
                     operational at all times, but a malfunction either on the
                     gateway or the new BEOS chain itself could result in BTS
                     funds being locked up temporarily until the problem is
-                    resolved.
+                    resolved. If you lose the keys to your BEOS account, the
+                    trustees cannot refund BTS locked up as BTS IOU tokens in
+                    your BEOS account, because the trustees cannot reclaim the
+                    IOU tokens and have no way to determine if the account is
+                    permanently lost.
                 </p>
                 <p>
-                    ** If you lose the keys to your BEOS account, the trustees
-                    cannot refund BTS locked up as BTS IOU tokens in your BEOS
-                    account because the trustees cannot reclaim the IOU tokens
-                    and have no way to determine if the account is permanently
-                    lost.
+                    Please read the full BLCA member agreement located at{" "}
+                    <a
+                        href="http://www.beos.world/membership_agreement.html"
+                        target="_blank"
+                    >
+                        http://www.beos.world/membership_agreement.html
+                    </a>{" "}
+                    before proceeding.
                 </p>
                 <p className="backingWarning">
                     By clicking on the button below, you are agreeing to the
