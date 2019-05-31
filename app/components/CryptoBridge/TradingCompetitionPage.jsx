63b7471304a262559eb2adab277e592225529a26
jquijino <jay.quijino@gmail.com>
2019-06-01 01:31:53 +0200

    Trading competition page layout & wording

--- app/components/CryptoBridge/TradingCompetitionPage.jsx
+++ app/components/CryptoBridge/TradingCompetitionPage.jsx
@@ -1,31 +1,64 @@
 import React from "react";
 import Translate from "react-translate-component";
+import AccountStore from "stores/AccountStore";
 
 class TradingCompetitionPage extends React.Component {
     render() {
+        const account = AccountStore.getState().currentAccount || "";
         return (
-            <div className="grid-block vertical">
-                <div className="grid-container">
-                    <div className="grid-content" style={{paddingTop: "2rem"}}>
+            <div>
+                <div className="padding">
+                    <Translate
+                        component="h2"
+                        content={"cryptobridge.competition.page.header"}
+                    />
+                    <Translate
+                        component="p"
+                        content={"cryptobridge.competition.page.intro"}
+                        unsafe
+                    />
+                </div>
+                <div className="grid-block vertical medium-horizontal">
+                    <div className="medium-6 padding">
+                        <iframe
+                            src={`https://widgets.crypto-bridge.org/leaderboard/?type=trading&pageSize=10&me=${account}`}
+                            style={{width: "100%", height: "600px"}}
+                            frameBorder="0"
+                        >
+                            Browser not compatible.
+                        </iframe>
+                    </div>
+                    <div className="medium-6 padding">
                         <Translate
-                            component="h2"
-                            content={"cryptobridge.competition.page.title"}
+                            component="h3"
+                            content={
+                                "cryptobridge.competition.page.leaderboard_title"
+                            }
                         />
                         <Translate
                             component="p"
-                            content={"cryptobridge.competition.page.intro"}
+                            content={
+                                "cryptobridge.competition.page.leaderboard_body1"
+                            }
                         />
-                        <div style={{height: "806px"}}>
-                            <iframe
-                                src={
-                                    "https://widgets.crypto-bridge.org/leaderboard/?pageSize=20&tabs=false"
-                                }
-                                style={{width: "100%", height: "800px"}}
-                                frameBorder="0"
+                        <Translate
+                            component="p"
+                            content={
+                                "cryptobridge.competition.page.leaderboard_body2"
+                            }
+                        />
+                        <p>
+                            <a
+                                href={`https://widgets.crypto-bridge.org/leaderboard/?type=trading&pageSize=10&me=${account}&modal=true`}
+                                target="_blank"
                             >
-                                Browser not compatible.
-                            </iframe>
-                        </div>
+                                <Translate
+                                    content={
+                                        "cryptobridge.competition.page.leaderboard_yourWidget"
+                                    }
+                                />
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
