38c0558ddc1e598bf52f43c2d9e6d00b727f58d7
Mokych Andrey <mokych.andrey@gmail.com>
2019-06-25 16:43:10 +0300

    #128 Removed charting_library sources and added script to extract it from remote assets folder

---
+++ charting_library/package-charting-library.js
@@ -0,0 +1,45 @@
+var fs = require("fs");
+var path = require("path");
+var archiver = require("archiver");
+const md5File = require("md5-file");
+
+
+var outputFile = path.join(__dirname, "charting_library.zip");
+const outFilename = path.basename(outputFile);
+var ignoreFiles = [path.basename(__filename), outFilename, outFilename + ".md5"];
+
+/**
+ * @param {String} source
+ * @param {String} out
+ * @returns {Promise}
+ */
+function zipDirectory(source, out) {
+    const archive = archiver("zip", { zlib: { level: 9 }});
+    const stream = fs.createWriteStream(out);
+
+    return new Promise((resolve, reject) => {
+        archive
+            .glob("**", {
+                cwd: source,
+                ignore: ignoreFiles,
+            })
+            .on("error", err => reject(err))
+            .pipe(stream);
+        stream.on("close", () => resolve());
+        archive.finalize();
+    });
+}
+
+function getMD5Digest(file) {
+    const hash = md5File.sync(file);
+    return hash.toUpperCase();
+}
+
+zipDirectory(__dirname, outputFile)
+    .then(() => {
+        console.log("Input folder compressed to " + outputFile);
+        var digest = getMD5Digest(outputFile);
+        console.log("MD5: " + digest);
+        fs.writeFileSync(outputFile + ".md5", digest);
+        process.exit(0);
+    });
