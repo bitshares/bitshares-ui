f87621b5892551ee6b4112b542904aa86890942f
y.kurets <yura.kurets@aetsoft.by>
2019-06-28 19:16:32 +0300

    #2813. hide account row if account isn't available

--- app/components/Account/AccountSelector.jsx
+++ app/components/Account/AccountSelector.jsx
@@ -231,9 +231,7 @@ class AccountSelector extends React.Component {
                 account.get("name")
             );
             account.accountType = this.getInputType(account.get("name"));
-            account.accountStatus =
-                ChainStore.getAccountMemberStatus(account) ||
-                "unknown";
+            account.accountStatus = ChainStore.getAccountMemberStatus(account);
             account.statusText = !account.isKnownScammer
                 ? counterpart.translate(
                       "account.member." + account.accountStatus
@@ -274,28 +272,30 @@ class AccountSelector extends React.Component {
                     if (this.props.excludeAccounts.indexOf(accountName) !== -1)
                         return null;
                     let account = ChainStore.getAccount(accountName);
-                    let account_status =
-                        ChainStore.getAccountMemberStatus(account) ||
-                        "unknown";
-                    let account_status_text = !accountUtils.isKnownScammer(
-                        accountName
-                    )
-                        ? "account.member." + account_status
-                        : "account.member.suspected_scammer";
-
-                    typeAheadAccounts.push({
-                        id: accountName,
-                        label: accountName,
-                        status: counterpart.translate(account_status_text),
-                        isOwn: myActiveAccounts.has(accountName),
-                        isFavorite: contacts.has(accountName),
-                        isKnownScammer: accountUtils.isKnownScammer(
+                    if (account) {
+                        let account_status =
+                            ChainStore.getAccountMemberStatus(account);
+                        let account_status_text = !accountUtils.isKnownScammer(
                             accountName
-                        ),
-                        className: accountUtils.isKnownScammer(accountName)
-                            ? "negative"
-                            : "positive"
-                    });
+                        )
+                            ? "account.member." + account_status
+                            : "account.member.suspected_scammer";
+
+                        typeAheadAccounts.push({
+                            id: accountName,
+                            label: accountName,
+                            status: counterpart.translate(account_status_text),
+                            isOwn: myActiveAccounts.has(accountName),
+                            isFavorite: contacts.has(accountName),
+                            isKnownScammer: accountUtils.isKnownScammer(
+                                accountName
+                            ),
+                            className: accountUtils.isKnownScammer(accountName)
+                                ? "negative"
+                                : "positive"
+                        });
+                    }
+                    return null;
                 })
                 .filter(a => !!a);
         }
@@ -308,28 +308,29 @@ class AccountSelector extends React.Component {
 
         if (!!accountName && !typeaheadHasAccount && this.state.inputChanged) {
             let _account = ChainStore.getAccount(accountName);
-            let _account_status = _account
-                ? ChainStore.getAccountMemberStatus(_account)
-                : null;
-            let _account_status_text = _account
-                ? !accountUtils.isKnownScammer(_account.get("name"))
-                    ? counterpart.translate("account.member." + _account_status)
-                    : counterpart.translate("account.member.suspected_scammer")
-                : counterpart.translate("account.errors.unknown");
+            if (_account) {
+                let _account_status = ChainStore.getAccountMemberStatus(_account);
+                let _account_status_text = _account
+                    ? !accountUtils.isKnownScammer(_account.get("name"))
+                        ? counterpart.translate("account.member." + _account_status)
+                        : counterpart.translate("account.member.suspected_scammer")
+                    : counterpart.translate("account.errors.unknown");
 
-            typeAheadAccounts.push({
-                id: this.props.accountName,
-                label: this.props.accountName,
-                status: _account_status_text,
-                isOwn: myActiveAccounts.has(accountName),
-                isFavorite: contacts.has(accountName),
-                isKnownScammer: accountUtils.isKnownScammer(accountName),
-                className:
-                    accountUtils.isKnownScammer(accountName) || !_account
-                        ? "negative"
-                        : null,
-                disabled: !_account ? true : false
-            });
+                typeAheadAccounts.push({
+                    id: this.props.accountName,
+                    label: this.props.accountName,
+                    status: _account_status_text,
+                    isOwn: myActiveAccounts.has(accountName),
+                    isFavorite: contacts.has(accountName),
+                    isKnownScammer: accountUtils.isKnownScammer(accountName),
+                    className:
+                        accountUtils.isKnownScammer(accountName) || !_account
+                            ? "negative"
+                            : null,
+                    disabled: !_account ? true : false
+                });
+            }
+
         }
 
         typeAheadAccounts.sort((a, b) => {
