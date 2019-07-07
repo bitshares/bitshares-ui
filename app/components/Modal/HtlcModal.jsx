19517e73fbe0226cfd1baa985cc381d35f3036a7
Andrey Mokych <mokych.andrey@gmail.com>
2019-07-07 11:56:30 +0300

    #139 Fixed expiration date mismatch between real HTLC and one, displayed in Extend modal and updated UI behavior a bit

--- app/components/Modal/HtlcModal.jsx
+++ app/components/Modal/HtlcModal.jsx
@@ -477,7 +477,13 @@ class HtlcModal extends React.Component {
                     preimage_hash:
                         operation.payload.conditions.hash_lock.preimage_hash[1],
                     preimage_size:
-                        operation.payload.conditions.hash_lock.preimage_hash[0]
+                        operation.payload.conditions.hash_lock.preimage_hash[0],
+                    expirationDate: moment(
+                        new Date(
+                            operation.payload.conditions.time_lock.expiration
+                        )
+                    ),
+                    period: null
                 });
             } else {
                 this.setState({
@@ -485,7 +491,13 @@ class HtlcModal extends React.Component {
                     preimage_hash:
                         operation.payload.conditions.hash_lock.preimage_hash[1],
                     preimage_size:
-                        operation.payload.conditions.hash_lock.preimage_hash[0]
+                        operation.payload.conditions.hash_lock.preimage_hash[0],
+                    expirationDate: moment(
+                        new Date(
+                            operation.payload.conditions.time_lock.expiration
+                        )
+                    ),
+                    period: null
                 });
             }
         } else {
