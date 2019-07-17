ed6b68dce1c496ccb16199ee4fedb64d9b1bedee
startail <startail@coondesign.eu>
2019-07-17 21:12:56 +0200

    Include custom staking + Checks if vesting starts at specific date + Adds View component for VestingBalance * Replaced unsafe method componentWillUpdate() - Removes unessesary components Tab, Tabs

--- app/components/Account/AccountVesting.jsx
+++ app/components/Account/AccountVesting.jsx
@@ -5,7 +5,129 @@ import {ChainStore} from "bitsharesjs";
 import utils from "common/utils";
 import WalletActions from "actions/WalletActions";
 import {Apis} from "bitsharesjs-ws";
-import {Tabs, Tab} from "../Utility/Tabs";
+import {Button} from "bitshares-ui-style-guide";
+
+function VestingBalanceView({
+    vestingAsset,
+    vestingType,
+    vestingId,
+    vestingBalance,
+    coinDaysEarned,
+    coinDaysRequired,
+    coinDaysRemaining,
+    availablePercent,
+    canClaim,
+    isCoinDays,
+    onClaim
+}) {
+    return (
+        <div>
+            <Translate
+                component="h5"
+                content="account.vesting.balance_number"
+                id={vestingId}
+            />
+
+            <table className="table key-value-table">
+                <tbody>
+                    <tr>
+                        <td>
+                            <Translate content="account.member.balance_type" />
+                        </td>
+                        <td>
+                            <Translate
+                                component="span"
+                                content={"account.vesting.type." + vestingType}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Translate content="account.member.cashback" />
+                        </td>
+                        <td>
+                            <FormattedAsset
+                                amount={vestingBalance}
+                                asset={vestingAsset}
+                            />
+                        </td>
+                    </tr>
+                    {coinDaysEarned ? (
+                        <tr>
+                            <td>
+                                <Translate content="account.member.earned" />
+                            </td>
+                            <td>
+                                {coinDaysEarned}
+                                &nbsp;
+                                <Translate
+                                    content={
+                                        isCoinDays
+                                            ? "account.member.coindays"
+                                            : "account.member.days"
+                                    }
+                                />
+                            </td>
+                        </tr>
+                    ) : null}
+                    {coinDaysRequired ? (
+                        <tr>
+                            <td>
+                                <Translate content="account.member.required" />
+                            </td>
+                            <td>
+                                {coinDaysRequired}
+                                &nbsp;
+                                <Translate
+                                    content={
+                                        isCoinDays
+                                            ? "account.member.coindays"
+                                            : "account.member.days"
+                                    }
+                                />
+                            </td>
+                        </tr>
+                    ) : null}
+                    {coinDaysRemaining ? (
+                        <tr>
+                            <td>
+                                <Translate content="account.member.remaining" />
+                            </td>
+                            <td>
+                                {coinDaysRemaining}
+                                &nbsp;
+                                <Translate content="account.member.days" />
+                            </td>
+                        </tr>
+                    ) : null}
+                    {availablePercent ? (
+                        <tr>
+                            <td>
+                                <Translate content="account.member.available" />
+                            </td>
+                            <td>
+                                {availablePercent * 100}% /{" "}
+                                <FormattedAsset
+                                    amount={availablePercent * vestingBalance}
+                                    asset={vestingAsset}
+                                />
+                            </td>
+                        </tr>
+                    ) : null}
+                    <tr>
+                        <td colSpan="2" style={{textAlign: "right"}}>
+                            {canClaim ? (
+                                <Button onClick={onClaim.bind(this, false)}>
+                                    <Translate content="account.member.claim" />
+                                </Button>
+                            ) : null}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
 
 class VestingBalance extends React.Component {
     _onClaim(claimAll, e) {
@@ -22,156 +144,104 @@ class VestingBalance extends React.Component {
 
     render() {
         let {vb} = this.props;
+
         if (!this.props.vb) {
             return null;
         }
 
         let cvbAsset,
-            secondsPerDay = 60 * 60 * 24,
-            balance;
-        let vestingPeriod = null;
-        let earned = null;
-        let availablePercent = null;
+            balance,
+            available_percentage = 0,
+            days_earned = 0,
+            days_required = 0,
+            days_remaining = 0,
+            isCoinDays = true,
+            canClaim = true;
+
         if (vb) {
             balance = vb.balance.amount;
             cvbAsset = ChainStore.getAsset(vb.balance.asset_id);
 
             if (vb.policy && vb.policy[0] !== 2) {
-                earned = vb.policy[1].coin_seconds_earned;
-                vestingPeriod = vb.policy[1].vesting_seconds;
-                availablePercent =
-                    vestingPeriod === 0
-                        ? 1
-                        : earned / (vestingPeriod * balance);
+                let start = Math.floor(
+                    new Date(vb.policy[1].start_claim + "Z").getTime() / 1000
+                );
+
+                if (start > 0) {
+                    // Vesting has a specific start date.
+                    // Vesting with locked value required to mautre fully before claiming
+                    // Full vesting period must pass before it can be claimed.
+                    // Calculate days left before a claim is possible
+                    // Example asset is BRIDGE.BCO - 1.3.1564
+
+                    let now = Math.floor(new Date().getTime() / 1000);
+                    let seconds_earned = now - start;
+                    let seconds_period = vb.policy[1].vesting_seconds;
+
+                    isCoinDays = false;
+                    if (seconds_earned < seconds_period) {
+                        canClaim = false;
+                        days_earned = parseFloat(
+                            seconds_earned / 86400
+                        ).toFixed(2);
+                        days_required = parseFloat(
+                            seconds_period / 86400
+                        ).toFixed(2);
+                        days_remaining = (days_required - days_earned).toFixed(
+                            2
+                        );
+                    }
+                } else {
+                    // Vesting has no start time.
+                    // Vesting balances has a vesting with maturing value
+                    // If period is 0 we expect a 100% claimable balance
+                    // otherwise we expect to be allowed to claim the matured percentage.
+
+                    let seconds_earned = vb.policy[1].coin_seconds_earned;
+                    let seconds_period = vb.policy[1].vesting_seconds;
+                    available_percentage =
+                        seconds_period === 0
+                            ? 1
+                            : seconds_earned / (seconds_period * balance);
+                    days_earned = utils.format_number(
+                        utils.get_asset_amount(
+                            seconds_earned / 86400,
+                            cvbAsset
+                        ),
+                        0
+                    );
+                    days_required = utils.format_number(
+                        utils.get_asset_amount(
+                            (vb.balance.amount * seconds_period) / 86400,
+                            cvbAsset
+                        ),
+                        0
+                    );
+                    days_remaining = utils.format_number(
+                        (seconds_period * (1 - available_percentage)) / 86400 ||
+                            0,
+                        2
+                    );
+                }
             }
         }
 
-        if (!cvbAsset) {
-            return null;
-        }
-
-        if (!balance) {
-            return null;
-        }
+        if (!cvbAsset || !balance) return null;
 
         return (
-            <div>
-                <Translate
-                    component="h5"
-                    content="account.vesting.balance_number"
-                    id={vb.id}
-                />
-
-                <table className="table key-value-table">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <Translate content="account.member.balance_type" />
-                            </td>
-                            <td>
-                                <span>{vb.balance_type}</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <Translate content="account.member.cashback" />
-                            </td>
-                            <td>
-                                <FormattedAsset
-                                    amount={vb.balance.amount}
-                                    asset={vb.balance.asset_id}
-                                />
-                            </td>
-                        </tr>
-                        {earned && (
-                            <tr>
-                                <td>
-                                    <Translate content="account.member.earned" />
-                                </td>
-                                <td>
-                                    {utils.format_number(
-                                        utils.get_asset_amount(
-                                            earned / secondsPerDay,
-                                            cvbAsset
-                                        ),
-                                        0
-                                    )}
-                                    &nbsp;
-                                    <Translate content="account.member.coindays" />
-                                </td>
-                            </tr>
-                        )}
-                        {earned && (
-                            <tr>
-                                <td>
-                                    <Translate content="account.member.required" />
-                                </td>
-                                <td>
-                                    {utils.format_number(
-                                        utils.get_asset_amount(
-                                            (vb.balance.amount *
-                                                vestingPeriod) /
-                                                secondsPerDay,
-                                            cvbAsset
-                                        ),
-                                        0
-                                    )}
-                                    &nbsp;
-                                    <Translate content="account.member.coindays" />
-                                </td>
-                            </tr>
-                        )}
-                        {earned && (
-                            <tr>
-                                <td>
-                                    <Translate content="account.member.remaining" />
-                                </td>
-                                <td>
-                                    {utils.format_number(
-                                        (vestingPeriod *
-                                            (1 - availablePercent)) /
-                                            secondsPerDay || 0,
-                                        2
-                                    )}
-                                    &nbsp;days
-                                </td>
-                            </tr>
-                        )}
-                        <tr>
-                            <td>
-                                <Translate content="account.member.available" />
-                            </td>
-                            {earned ? (
-                                <td>
-                                    {utils.format_number(
-                                        availablePercent * 100,
-                                        2
-                                    )}
-                                    % /{" "}
-                                    <FormattedAsset
-                                        amount={
-                                            availablePercent * vb.balance.amount
-                                        }
-                                        asset={cvbAsset.get("id")}
-                                    />
-                                </td>
-                            ) : (
-                                <td>{utils.format_number(100, 2)}%</td>
-                            )}
-                        </tr>
-                        <tr>
-                            <td colSpan="2" style={{textAlign: "right"}}>
-                                <button
-                                    onClick={this._onClaim.bind(this, false)}
-                                    className="button"
-                                >
-                                    <Translate content="account.member.claim" />
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <VestingBalanceView
+                vestingId={vb.id}
+                vestingAsset={vb.balance.asset_id}
+                vestingType={vb.balance_type}
+                vestingBalance={vb.balance.amount}
+                coinDaysEarned={days_earned}
+                coinDaysRequired={days_required}
+                coinDaysRemaining={days_remaining}
+                availablePercent={available_percentage}
+                canClaim={canClaim}
+                isCoinDays={isCoinDays}
+                onClaim={this._onClaim.bind(this)}
+            />
         );
     }
 }
@@ -181,7 +251,7 @@ class AccountVesting extends React.Component {
         super();
 
         this.state = {
-            vbs: null
+            vesting_balances: null
         };
     }
 
@@ -189,8 +259,8 @@ class AccountVesting extends React.Component {
         this.retrieveVestingBalances.call(this, this.props.account.get("id"));
     }
 
-    componentWillUpdate(nextProps) {
-        let newId = nextProps.account.get("id");
+    componentDidUpdate(prevProps) {
+        let newId = prevProps.account.get("id");
         let oldId = this.props.account.get("id");
 
         if (newId !== oldId) {
@@ -203,8 +273,8 @@ class AccountVesting extends React.Component {
         Apis.instance()
             .db_api()
             .exec("get_vesting_balances", [accountId])
-            .then(vbs => {
-                this.setState({vbs});
+            .then(vesting_balances => {
+                this.setState({vesting_balances});
             })
             .catch(err => {
                 console.log("error:", err);
@@ -212,25 +282,23 @@ class AccountVesting extends React.Component {
     }
 
     render() {
-        let {vbs} = this.state;
+        let {vesting_balances} = this.state;
         if (
-            !vbs ||
+            !vesting_balances ||
             !this.props.account ||
             !this.props.account.get("vesting_balances")
         ) {
             return null;
         }
 
-        let account = this.props.account.toJS();
-
-        let balances = vbs
+        let balances = vesting_balances
             .map(vb => {
                 if (vb.balance.amount) {
                     return (
                         <VestingBalance
                             key={vb.id}
                             vb={vb}
-                            account={account}
+                            account={this.props.account.toJS()}
                             handleChanged={this.retrieveVestingBalances.bind(
                                 this
                             )}
@@ -244,34 +312,28 @@ class AccountVesting extends React.Component {
 
         return (
             <div className="grid-content app-tables no-padding" ref="appTables">
-                <div className="content-block small-12">
-                    <div className="tabs-container generic-bordered-box">
-                        <Tabs
-                            segmented={false}
-                            setting="vestingTab"
-                            className="account-tabs"
-                            tabsClass="account-overview bordered-header content-block"
-                            contentClass="padding"
-                        >
-                            <Tab title="account.vesting.title">
+                <div
+                    className="grid-block main-content margin-block wrap"
+                    style={{margin: 0}}
+                >
+                    <div className="grid-content">
+                        <Translate
+                            component="h1"
+                            content="account.vesting.title"
+                        />
+                        <Translate
+                            content="account.vesting.explain"
+                            component="p"
+                        />
+                        {!balances.length ? (
+                            <h4 style={{paddingTop: "1rem"}}>
                                 <Translate
-                                    content="account.vesting.explain"
-                                    component="p"
+                                    content={"account.vesting.no_balances"}
                                 />
-
-                                {!balances.length ? (
-                                    <h4 style={{paddingTop: "1rem"}}>
-                                        <Translate
-                                            content={
-                                                "account.vesting.no_balances"
-                                            }
-                                        />
-                                    </h4>
-                                ) : (
-                                    balances
-                                )}
-                            </Tab>
-                        </Tabs>
+                            </h4>
+                        ) : (
+                            balances
+                        )}
                     </div>
                 </div>
             </div>
