aa8c6f2d81e83351099485b86518ec6a0a82c7f4
Mrugakshee Palwe <mru9aksheepalwe@gmail.com>
2019-07-22 14:00:45 -0400

    2837-KeyImportFromWIF - Fix UI breakage in WIF key field - Add error messages for incorrect WIF - Add translations for error messages - Display associated account(s) for WIF

--- app/api/accountApi.js
+++ app/api/accountApi.js
@@ -6,6 +6,11 @@ class Api {
             .db_api()
             .exec("lookup_accounts", [startChar, limit]);
     }
+    lookupAccountByPublicKey(pubKey) {
+        return Apis.instance()
+            .db_api()
+            .exec("get_key_references", [[pubKey]]);
+    }
 }
 
 export default new Api();
