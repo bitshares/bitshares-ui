8e3cad430d72e7e46fa9909ac241839947ee026e
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2019-07-30 10:57:03 +0200

    add comment
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- app/api/accountApi.js
+++ app/api/accountApi.js
@@ -2,11 +2,13 @@ import {Apis} from "bitsharesjs-ws";
 
 class Api {
     lookupAccounts(startChar, limit) {
+        // direct query, circumventing subscription system of ChainStore
         return Apis.instance()
             .db_api()
             .exec("lookup_accounts", [startChar, limit]);
     }
     lookupAccountByPublicKey(pubKey) {
+        // direct query, circumventing subscription system of ChainStore
         return Apis.instance()
             .db_api()
             .exec("get_key_references", [[pubKey]]);
