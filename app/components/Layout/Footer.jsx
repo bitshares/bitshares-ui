9bb4c5a961d3845d9af55f65b3b9ea1ce083bc8f
Abit <abitmore@users.noreply.github.com>
2021-05-16 12:40:55 +0200

    Update download link

--- app/components/Layout/Footer.jsx
+++ app/components/Layout/Footer.jsx
@@ -82,7 +82,7 @@ class Footer extends React.Component {
     componentDidMount() {
         this.checkNewVersionAvailable.call(this);
 
-        this.downloadLink = "https://bitshares.org/download";
+        this.downloadLink = "https://github.com/bitshares/bitshares-ui/releases/latest";
 
         let ensure = this._ensureConnectivity.bind(this);
         ifvisible.on("wakeup", function() {
