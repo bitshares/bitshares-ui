666a1b794271227f9e81acb325bd8e5e64bdbaea
Ammar Yousef (ioBanker) <82454470+AmmarYousefM@users.noreply.github.com>
2021-07-09 16:36:51 +0300

    Update AssetStore.js

--- app/stores/AssetStore.js
+++ app/stores/AssetStore.js
@@ -14,7 +14,8 @@ class AssetStore extends BaseStore {
 
         this.bindListeners({
             onGetAssetList: AssetActions.getAssetList,
-            onLookupAsset: AssetActions.lookupAsset
+            onLookupAsset: AssetActions.lookupAsset,
+            onGetAssetsByIssuer: AssetActions.getAssetsByIssuer
         });
     }
 
@@ -55,6 +56,45 @@ class AssetStore extends BaseStore {
             });
         }
     }
+
+    onGetAssetsByIssuer(payload) {
+        if (!payload) {
+            return false;
+        }
+        this.assetsLoading = payload.loading;
+
+        if (payload.assets) {
+            payload.assets.forEach(asset => {
+                for (var i = 0; i < payload.dynamic.length; i++) {
+                    if (payload.dynamic[i].id === asset.dynamic_asset_data_id) {
+                        asset.dynamic = payload.dynamic[i];
+                        break;
+                    }
+                }
+
+//                if (asset.bitasset_data_id) {
+//                    asset.market_asset = true;
+//
+//                    for (var i = 0; i < payload.bitasset_data.length; i++) {
+//                        if (
+//                            payload.bitasset_data[i].id ===
+//                            asset.bitasset_data_id
+//                        ) {
+//                            asset.bitasset_data = payload.bitasset_data[i];
+//                            break;
+//                        }
+//                    }
+//                } else {
+//                    asset.market_asset = false;
+//                }
+
+                this.assets = this.assets.set(asset.id, asset);
+
+                this.asset_symbol_to_id[asset.symbol] = asset.id;
+            });
+        }
+    }
+
 
     onLookupAsset(payload) {
         this.searchTerms[payload.searchID] = payload.symbol;
