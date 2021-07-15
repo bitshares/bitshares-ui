27fc6599239d8f69c55f4fe9b1d5f826f532bc2b
freak5g <87205099+freak5g@users.noreply.github.com>
2021-07-15 15:37:49 +0200

    Update CreateAccountPassword.jsx

--- app/components/Account/CreateAccountPassword.jsx
+++ app/components/Account/CreateAccountPassword.jsx
@@ -270,7 +270,7 @@ class CreateAccountPassword extends React.Component {
                                 title={
                                     <div
                                         dangerouslySetInnerHTML={{
-                                            __html: counterpart.translate("tooltip.generate")
+                                        __html: counterpart.translate("tooltip.generate")
                                         }}
                                     />
                                 }
