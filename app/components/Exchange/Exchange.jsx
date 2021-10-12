9f84fb126cc1e62aaeaa76c93310ea022ff89af9
Ammar Yousef (ioBanker) <82454470+AmmarYousefM@users.noreply.github.com>
2021-10-12 21:06:38 +0300

    Update Exchange.jsx

--- app/components/Exchange/Exchange.jsx
+++ app/components/Exchange/Exchange.jsx
@@ -344,7 +344,7 @@ class Exchange extends React.Component {
             hideScrollbars: ws.get("hideScrollbars", false),
             singleColumnOrderForm: ws.get("singleColumnOrderForm", true),
             flipOrderBook: ws.get("flipOrderBook", false),
-            flipBuySell: ws.get("flipBuySell", true),
+            flipBuySell: ws.get("flipBuySell", false),
             orderBookReversed: ws.get("orderBookReversed", false),
             chartType: ws.get("chartType", "price_chart"),
             chartHeight: chart_height,
