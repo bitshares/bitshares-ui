f76b8bcbe6d18c343d4ac2d7a0f023072ad362e3
Ammar Yousef (ioBanker) <82454470+AmmarYousefM@users.noreply.github.com>
2022-02-01 01:47:55 +0300

    Removing unmatched background from Markets page (#3392)
    * Removing unmatched background from Markets page

--- app/components/Exchange/Markets.jsx
+++ app/components/Exchange/Markets.jsx
@@ -28,7 +28,6 @@ class Markets extends React.Component {
 
     _setDimensions() {
         let height = this.refs.wrapper.offsetHeight;
-
         if (height !== this.state.height) {
             this.setState({height});
         }
@@ -36,7 +35,7 @@ class Markets extends React.Component {
 
     render() {
         return (
-            <div ref="wrapper" className="grid-block page-layout no-overflow">
+            <div ref="wrapper" className="grid-block no-overflow">
                 <MyMarkets
                     style={{width: "100%", padding: 20}}
                     listHeight={this.state.height ? this.state.height : null}
