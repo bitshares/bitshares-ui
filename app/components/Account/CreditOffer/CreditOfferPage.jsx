634813828b015d544cb75dc771eec63df9242bb3
Ihor Brazhnichenko <ihor.bra@gmail.com>
2022-11-23 05:30:53 +0200

    #3568 Show borrow window for owner for testing purpose and prevent submit with owner error

--- app/components/Account/CreditOffer/CreditOfferPage.jsx
+++ app/components/Account/CreditOffer/CreditOfferPage.jsx
@@ -92,23 +92,6 @@ class CreditOfferPage extends React.Component {
     }
 
     showAcceptModal(data) {
-        // console.log("TODO:", data);
-        let {currentAccount} = this.props;
-        let account = ChainStore.getAccount(currentAccount);
-        if (account.get("id") == data.owner_account) {
-            notify.addNotification.defer({
-                children: (
-                    <Translate
-                        component="span"
-                        content="credit_offer.info_borrow_err"
-                    />
-                ),
-                level: "error",
-                autoDismiss: 3
-            });
-            return;
-        }
-
         let assetList = data.acceptable_collateral.map(v => v[0]);
         let debtAsset = data.asset_type;
         let selectAsset = assetList[0];
@@ -468,7 +451,7 @@ class CreditOfferPage extends React.Component {
         let maxReal = maxAssetAmount.getAmount({real: true});
         let maxError = amount > maxReal || maxReal <= 0;
         const isSubmitNotValid =
-            !amount || minError || maxError || !selectAsset || balanceError;
+            !amount || minError || maxError || !selectAsset || balanceError || account.get("id") == info.owner_account;
         let _error = maxError ? "has-error" : "";
         if (currentBalance && currentBalance > 0) {
             balance = (
@@ -517,6 +500,12 @@ class CreditOfferPage extends React.Component {
                 overlay={true}
                 onCancel={this.hideAcceptModal}
                 footer={[
+                    info.owner_account === info.owner_account && (
+                            <Translate
+                                component="span"
+                                content="credit_offer.info_borrow_err"
+                            />
+                    ) || null,
                     <Button
                         key={"send"}
                         disabled={isSubmitNotValid}
