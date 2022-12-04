41224171d216788b34ec3d03cb7121db17691ad4
ioBanker <37595908+ioBanker@users.noreply.github.com>
2022-12-04 03:13:30 +0300

    Update AssetActions.js

--- app/actions/AssetActions.js
+++ app/actions/AssetActions.js
@@ -203,6 +203,35 @@ class AssetActions {
         };
     }
 
+    claimCollateralFees(account_id, asset, backingAsset, claimFeesAmountAsset) {
+        let tr = WalletApi.new_transaction();
+
+        tr.add_type_operation("asset_claim_fees", {
+            fee: {
+                amount: 0,
+                asset_id: 0
+            },
+            issuer: account_id,
+            amount_to_claim: {
+                asset_id: backingAsset.asset_id,
+                amount: claimFeesAmountAsset.getAmount()
+            },
+            extensions: {
+                claim_from_asset_id: asset.get("id")
+            }
+        });
+        return dispatch => {
+            return WalletDb.process_transaction(tr, null, true)
+                .then(() => {
+                    dispatch(true);
+                })
+                .catch(error => {
+                    console.log("----- claimFees error ----->", error);
+                    dispatch(false);
+                });
+        };
+    }
+
     assetGlobalSettle(asset, account_id, price) {
         let tr = WalletApi.new_transaction();
 
